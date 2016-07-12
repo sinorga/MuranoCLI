b313ea7030f10462b09e43f08e7f6ac48d5f98ca
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-07-12 15:36:48 -0500

    Massive reshaping to take advantage of ruby.

--- lib/MrMurano/Solution.rb
+++ lib/MrMurano/Solution.rb
@@ -5,19 +5,127 @@ require 'date'
 require 'pp'
 
 module MrMurano
-  class Solution
-    def endPoint(path)
-      URI('https://' + $cfg['net.host'] + '/api:1/solution/' +
-          $cfg['solution.id'] + path.to_s)
+  class SolutionBase
+    def initialize
+      @token = Account.new.token
+      @sid = $cfg['solution.id']
+      raise "No solution!" if @sid.nil?
+      @uriparts = [:solution, @sid]
     end
 
-    def version
-      r = endPoint('/version')
-      Net::HTTP.start(r.host, r.port, :use_ssl=>true) do |http|
-        request = Net::HTTP::Get.new(r)
-        request.content_type = 'application/json'
-        request['authorization'] = 'token ' + token
+    def endPoint(path='')
+      parts = ['https:/', $cfg['net.host'], 'api:1'] + @uriparts
+      s = parts.map{|v| v.to_s}.join('/')
+      URI(s + path.to_s)
+    end
+    def http
+      uri = URI('https://' + $cfg['net.host'])
+      if @http.nil? then
+        @http = Net::HTTP.new(uri.host, uri.port)
+        @http.use_ssl = true
+        @http.start
+      end
+      @http
+    end
 
+    def set_req_defaults(request)
+      request.content_type = 'application/json'
+      request['authorization'] = 'token ' + @token
+      request
+    end
+
+    def workit(request, &block)
+      set_req_defaults(request)
+      if block_given? then
+        yield request, http()
+      else
+        response = http().request(request)
+        case response
+        when Net::HTTPSuccess
+          return JSON.parse(response.body)
+        else
+          raise response
+        end
+      end
+    end
+
+    def get(path='', &block)
+      uri = endPoint(path)
+      workit(Net::HTTP::Get.new(uri), &block) 
+    end
+
+    def post(path='', &block)
+      uri = endPoint(path)
+      workit(Net::HTTP::Post.new(uri), &block)
+    end
+
+    def put(path='', &block)
+      uri = endPoint(path)
+      workit(Net::HTTP::Put.new(uri))
+    end
+
+    def delete(path='', &block)
+      uri = endPoint(path)
+      workit(Net::HTTP::Delete.new(uri))
+    end
+
+    # …
+
+  end
+  class Solution < SolutionBase
+    def version
+      get('/version')
+    end
+
+    def info
+      get()
+    end
+
+    # …/serviceconfig
+    def sc
+      get('/serviceconfig/')
+    end
+    #
+  end
+
+  # …/file 
+  class File < SolutionBase
+    def initialize
+      super
+      @uriparts << 'file'
+    end
+
+    ##
+    # Get a list of all of the static content
+    def list
+      get()
+    end
+
+    ##
+    # Get one item of the static content.
+    def fetch(path, &block)
+      get('/'+path) do |request, http|
+        response = http.request(request)
+        case response
+        when Net::HTTPSuccess
+          #if block_given? then
+          #  response.read_body &block
+          #else
+            say_error "Kl"
+            response.read_body do |chunk|
+              $stdout.write chunk
+            end
+          #end
+        else
+          raise response
+        end
+      end
+    end
+
+    ##
+    # Delete a file
+    def remove(path)
+      delete('/'+path) do |request, http|
         response = http.request(request)
         case response
         when Net::HTTPSuccess
@@ -28,6 +136,39 @@ module MrMurano
         end
       end
     end
+
+  end
+
+  # …/role
+  # …/user
+
+  # …/endpoint
+  class Endpoint < SolutionBase
+    def initialize
+      super
+      @uriparts << 'endpoint'
+    end
+
+    ##
+    # This gets all data about all endpoints
+    def list
+      get()
+    end
+
+    # create
+    # delete
+    #
+  end
+
+end
+
+command :solution do |c|
+  c.syntax = %{mr solution ...}
+
+  c.action do |args, options|
+
+    sol = MrMurano::File.new
+    say sol.fetch('')
 
   end
 end
