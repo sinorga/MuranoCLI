5169a42b35b5600729f19d2e74bce6b9f08e5700
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-09-12 14:26:18 -0500

    breask assign into two seperate commands.

--- lib/MrMurano/Solution-ServiceConfig.rb
+++ lib/MrMurano/Solution-ServiceConfig.rb
@@ -51,51 +51,55 @@ module MrMurano
   end
 end
 
-command :assign do |c|
-  c.syntax = 'mr assign [product]'
-  c.description = 'Assign a product to a eventhandler'
-
-  c.option '--list', %{List assigned products}
+command 'assign list' do |c|
+  c.syntax = 'mr assign list [options]'
+  c.description = 'List the products that are assigned'
   c.option '--idonly', 'Only return the ids'
+  c.action do |args, options|
+    sol = MrMurano::SC_Device.new
+
+    trigs = sol.showTriggers()
+    if options.idonly or $cfg['business.id'].nil? then
+      say trigs.join(' ')
+    else
+      acc = MrMurano::Account.new
+      products = acc.products
+      products.select!{|p| trigs.include? p[:modelId] }
+      if products.empty? then
+        say trigs.join(' ')
+      else
+        busy = products.map{|r| [r[:label], r[:type], r[:pid], r[:modelId]]}
+        table = Terminal::Table.new :rows => busy, :headings => ['Label', 'Type', 'PID', 'ModelID']
+        say table
+      end
+    end
+  end
+end
+alias_command :assign, 'assign list'
+
+command 'assign set' do |c|
+  c.syntax = 'mr assign set [product]'
+  c.description = 'Assign a product to a eventhandler'
 
   c.action do |args, options|
     sol = MrMurano::SC_Device.new
 
-    if options.list then
-      trigs = sol.showTriggers()
-      if options.idonly or $cfg['business.id'].nil? then
-        say trigs.join(' ')
-      else
-        acc = MrMurano::Account.new
-        products = acc.products
-        products.select!{|p| trigs.include? p[:modelId] }
-        if products.empty? then
-          say trigs.join(' ')
-        else
-          busy = products.map{|r| [r[:label], r[:type], r[:pid], r[:modelId]]}
-          table = Terminal::Table.new :rows => busy, :headings => ['Label', 'Type', 'PID', 'ModelID']
-          say table
-        end
-      end
-
+    prname = args.shift
+    if prname.nil? then
+      prid = $cfg['product.id']
     else
-      prname = args.shift
-      if prname.nil? then
-        prid = $cfg['product.id']
-      else
-        acc = MrMurano::Account.new
-        products = acc.products
-        products.select!{|p|
-          p[:label] == prname or p[:modelId] == prname or p[:pid] == prname
-        }
-        prid = products.map{|p| p[:modelId]}
-      end
-      raise "No product ID!" if prid.nil?
-      say "Assigning #{prid} to solution" if $cfg['tool.verbose']
-      sol.assignTriggers(prid) unless $cfg['tool.dry']
+      acc = MrMurano::Account.new
+      products = acc.products
+      products.select!{|p|
+        p[:label] == prname or p[:modelId] == prname or p[:pid] == prname
+      }
+      prid = products.map{|p| p[:modelId]}
     end
-
+    raise "No product ID!" if prid.nil?
+    say "Assigning #{prid} to solution" if $cfg['tool.verbose']
+    sol.assignTriggers(prid) unless $cfg['tool.dry']
   end
+
 end
 
 #  vim: set ai et sw=2 ts=2 :
