fc24bf44578f15cce74ab49828618f54d946e5f3
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-09-13 14:17:35 -0500

    Save password from .Solution.secret

--- lib/MrMurano/configFile.rb
+++ lib/MrMurano/configFile.rb
@@ -277,22 +277,40 @@ command 'config import' do |c|
     solfile = ($cfg['location.base'] + 'Solutionfile.json')
     solsecret = ($cfg['location.base'] + '.Solutionfile.secret')
 
-    if solfile.exists? then
+    if solfile.exist? then
       # Is in JSON, which as a subset of YAML, so use YAML parser
       solfile.open do |io|
         sf = YAML.load(io)
         $cfg.set('location.files', sf['assets']) if sf.has_key? 'assets'
         $cfg.set('location.files', sf['file_dir']) if sf.has_key? 'file_dir'
         $cfg.set('files.default_page', sf['default_page']) if sf.has_key? 'default_page'
+
+        # TODO: other bits.
       end
     end
 
-    if solsecret.exists? then
+    if solsecret.exist? then
       # Is in JSON, which as a subset of YAML, so use YAML parser
       solsecret.open do |io|
         ss = YAML.load(io)
-        ss['email']
-        ss['password']
+
+        pff = Pathname.new(ENV['HOME']) + '.mrmurano/passwords'
+        pwd = MrMurano::Passwords.new(pff)
+        pwd.load
+        ps = pwd.get($cfg['net.host'], ss['email'])
+        if ps.nil? then
+          pwd.set($cfg['net.host'], ss['email'], ss['password'])
+          pwd.save
+        elsif ps != ss['password'] then
+          y = ask("A different password for this account already exists. Overwrite? N/y")
+          if y =~ /^y/i then
+            pwd.set($cfg['net.host'], ss['email'], ss['password'])
+            pwd.save
+          end
+        else
+          # already set, nothing to do.
+        end
+
         $cfg.set('solution.id', ss['solution_id']) if ss.has_key? 'solution_id'
         $cfg.set('solution.id', ss['product_id']) if ss.has_key? 'product_id'
       end
