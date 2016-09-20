bfdf5c76812ceb0498b070ffeb169e47f8c3dddc
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-09-20 08:45:37 -0500

    Add get/set tests

--- spec/Config_spec.rb
+++ spec/Config_spec.rb
@@ -3,17 +3,57 @@ require 'MrMurano/Config'
 require 'tempfile'
 
 RSpec.describe MrMurano::Config do
-  it "Sets defaults" do
-    cfg = MrMurano::Config.new
-    cfg.load
-    # Don't check for all of them, just a few.
-    expect(cfg['files.default_page']).to eq('index.html')
-    expect(cfg.get('files.default_page', :defaults)).to eq('index.html')
-    expect(cfg['tool.debug']).to eq(false)
-    expect(cfg.get('tool.debug', :defaults)).to eq(false)
-  end
+  context "Basics " do
+    before(:example) do
+      @tmpdir = Dir.tmpdir
+      @projectDir = @tmpdir + '/home/work/project'
+      FileUtils.mkpath(@projectDir)
+      # Set ENV to override output of Dir.home
+      ENV['HOME'] = @tmpdir + '/home'
+    end
 
-  # TODO: other tests.
+    after(:example) do
+      FileUtils.remove_dir(@tmpdir + '/home', true) if FileTest.exist? @tmpdir
+    end
+
+    it "Sets defaults" do
+      cfg = MrMurano::Config.new
+      cfg.load
+      # Don't check for all of them, just a few.
+      expect(cfg['files.default_page']).to eq('index.html')
+      expect(cfg.get('files.default_page', :defaults)).to eq('index.html')
+      expect(cfg['tool.debug']).to eq(false)
+      expect(cfg.get('tool.debug', :defaults)).to eq(false)
+    end
+
+    it "Sets internal values" do
+      cfg = MrMurano::Config.new
+      cfg.load
+
+      cfg['bob.test'] = 'twelve'
+
+      expect(cfg['bob.test']).to eq('twelve')
+      expect(cfg.get('bob.test', :internal)).to eq('twelve')
+    end
+
+    it "Sets project values" do # This should write
+      cfg = MrMurano::Config.new
+      cfg.load
+
+      cfg.set('bob.test', 'twelve', :project)
+
+      expect(cfg['bob.test']).to eq('twelve')
+      expect(cfg.get('bob.test', :project)).to eq('twelve')
+
+      expect(FileTest.exist?(@projectDir + '.mrmuranorc'))
+
+      #reload
+      cfg = MrMurano::Config.new
+      cfg.load
+      expect(cfg.get('bob.test', :project)).to eq('twelve')
+    end
+
+  end
 
   context "Can find the project directory by .mrmuranorc" do
     before(:example) do
