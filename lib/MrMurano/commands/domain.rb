e944b6cdd289855f245380fb98d543914393d0f8
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-09-26 08:27:05 -0500

    Add command to get the domain for this solution

---
+++ lib/MrMurano/commands/domain.rb
@@ -0,0 +1,17 @@
+require 'MrMurano/Solution'
+command :domain do |c|
+  c.syntax = %{mr domain}
+  c.summary = %{Print the domain for this solution}
+  c.option '--[no-]raw', %{Don't add scheme}
+  c.action do |args,options|
+    options.defaults :raw=>true
+    sol = MrMurano::Solution.new
+    ret = sol.info()
+    if options.raw then
+      say ret[:domain]
+    else
+      say "https://#{ret[:domain]}"
+    end
+  end
+end
+#  vim: set ai et sw=2 ts=2 :
