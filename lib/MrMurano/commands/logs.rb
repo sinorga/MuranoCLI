4efaec4b7a8717c0c7168b1d61ad2fd868572cbf
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-10-14 08:40:32 -0500

    Fix #12  log prettying bug.
    makePretty() was assuming that certain keys existed.  Turns out they don't have to.  So rebuilt to handle any of the assumed keys to be missing.
    
    Then added a --raw option just in case.
    
    Also dropped --pollrate, because it hasn't been used for a while.

--- lib/MrMurano/commands/logs.rb
+++ lib/MrMurano/commands/logs.rb
@@ -6,62 +6,65 @@ command :logs do |c|
   c.syntax = %{mr logs [options]}
   c.description = %{Get the logs for a solution}
   c.option '-f','--follow', %{Follow logs from server}
-  c.option '--pollrate RATE', Integer, %{Seconds to sleep between polls}
   c.option('--[no-]color', %{Toggle colorizing of logs}) {
     Rainbow.enabled = false
   }
   c.option '--[no-]pretty', %{Reformat JSON blobs in logs.}
   c.option '--[no-]localtime', %{Adjust Timestamps to be in local time}
+  c.option '--raw', %{Don't do any formating of the log data}
 
   c.action do |args,options|
-    options.default :pretty=>true, :localtime=>true, :pollrate=>5
+    options.default :pretty=>true, :localtime=>true, :raw => false
 
-    lasttime = ""
+    def makeJsonPretty(data, options)
+      if options.pretty then
+        ret = JSON.pretty_generate(data).to_s
+        ret[0] = ret[0].color(:magenta)
+        ret[-1] = ret[-1].color(:magenta)
+        ret
+      else
+        data.to_json
+      end
+    end
 
     def makePretty(line, options)
       out=''
-      if line.has_key?(:type) then
-        out << "#{line[:type]} ".upcase.color(:red).background(:aliceblue)
-      end
-      out << "[#{line[:subject]}]".color(:red).background(:aliceblue)
+      out << "#{line[:type] || '--'} ".upcase.color(:red).background(:aliceblue)
+      out << "[#{line[:subject] || ''}]".color(:red).background(:aliceblue)
       out << " "
-      if options.localtime then
-        curtime = Time.at(line[:timestamp]).localtime.to_datetime.iso8601(3)
+      if line.has_key?(:timestamp) then
+        if options.localtime then
+          curtime = Time.at(line[:timestamp]).localtime.to_datetime.iso8601(3)
+        else
+          curtime = Time.at(line[:timestamp]).to_datetime.iso8601(3)
+        end
       else
-        curtime = Time.at(line[:timestamp]).to_datetime.iso8601(3)
+        curtime = "<no timestamp>"
       end
       out << curtime.color(:blue)
       out << ":\n"
       if line.has_key?(:data) then
         data = line[:data]
 
-        if data.kind_of?(Hash) and data.has_key?(:request) and data.has_key?(:response) then
-          out << "---------\nrequest:"
-          if options.pretty then
-            ret = JSON.pretty_generate(data[:request]).to_s
-            ret[0] = ret[0].color(:magenta)
-            ret[-1] = ret[-1].color(:magenta)
-            out << ret
-          else
-            out << data[:request].to_json
-          end
+        if data.kind_of?(Hash) then
+          if data.has_key?(:request) and data.has_key?(:response) then
+            out << "---------\nrequest:"
+            out << makeJsonPretty(data[:request], options)
 
-          out << "\n---------\nresponse:"
-          if options.pretty then
-            ret = JSON.pretty_generate(data[:response]).to_s
-            ret[0] = ret[0].color(:magenta)
-            ret[-1] = ret[-1].color(:magenta)
-            out << ret
+            out << "\n---------\nresponse:"
+            out << makeJsonPretty(data[:response], options)
           else
-            out << data[:response].to_json
+            out << makeJsonPretty(data, options)
           end
-
         else
           out << data.to_s
         end
 
       else
-        out << line.to_s
+        line.delete :type
+        line.delete :timestamp
+        line.delete :subject
+        out << makeJsonPretty(line, options)
       end
       out
     end
@@ -80,11 +83,15 @@ command :logs do |c|
 
               # for all complete JSON blobs, make them pretty.
               chunk.gsub!(/\{(?>[^}{]+|\g<0>)*\}/m) do |m|
-                js = JSON.parse(m, {:allow_nan=>true,
-                                    :symbolize_names => true,
-                                    :create_additions=>false})
-                puts makePretty(js, options)
-                '' #remove
+                if options.raw then
+                  puts m
+                else
+                  js = JSON.parse(m, {:allow_nan=>true,
+                                      :symbolize_names => true,
+                                      :create_additions=>false})
+                  puts makePretty(js, options)
+                end
+                '' #remove (we're kinda abusing gsub here.)
               end
 
               # is there an incomplete one?
@@ -95,7 +102,7 @@ command :logs do |c|
             end
           end
         end
-      rescue Interrupt => e
+      rescue Interrupt => _
       end
 
     else
@@ -103,7 +110,11 @@ command :logs do |c|
 
       if ret.kind_of?(Hash) and ret.has_key?(:items) then
         ret[:items].reverse.each do |line|
-          puts makePretty(line, options)
+          if options.raw then
+            puts line
+          else
+            puts makePretty(line, options)
+          end
         end
       else
         say_error "Couldn't get logs: #{ret}"
