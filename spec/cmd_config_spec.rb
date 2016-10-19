11c7513c45945d0552c7ceafe276a9cfe64ee797
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-10-19 09:55:30 -0500

    Testing with custom config data

--- spec/cmd_config_spec.rb
+++ spec/cmd_config_spec.rb
@@ -1,21 +1,36 @@
 
 RSpec.describe 'mr config' do
+  def read_temp(path)
+    puts temp_path
+    IO.read(File.join(temp_path, path))
+  end
+
   describe "Needs a key" do
     command %w{mr config}
     its(:stdout) { is_expected.to include('Need a config key')}
+    its(:stderr) { is_expected.to eq('')}
   end
 
   describe "Set a key" do
     command %w{mr config bob build}
     its(:stdout) { is_expected.to eq('')}
+    its(:stderr) { is_expected.to eq('')}
   end
+
   describe "Reads a key" do
-    command %w{mr config test.bob}
-    file '.mrmuranorc', <<-EOH
-    [test]
-    bob=build
-    EOH
+    command %w{mr config --project doThisTest.bob}
+    fixture_file '.mrmuranorc'
     its(:stdout) { is_expected.to include('build')}
+    its(:stderr) { is_expected.to eq('')}
+  end
+
+  describe "Removes a key" do
+    command %{mr config --project --unset doThisTest.bob}
+    fixture_file '.mrmuranorc'
+    its(:stdout) { is_expected.to eq('')}
+    its(:stderr) { is_expected.to eq('')}
+    it { is_expected.to match_fixture 'mrmuranorc_deleted_bob' }
+    #it {expect(read_temp('.mrmuranorc')).to eq('[test]')}
   end
 end
 
