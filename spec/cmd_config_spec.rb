083940cba99de2f6721ab6a7f38a53731f28c32d
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-10-19 09:19:25 -0500

    Adding `mr config` tests

--- spec/cmd_config_spec.rb
+++ spec/cmd_config_spec.rb
@@ -1,7 +1,22 @@
 
 RSpec.describe 'mr config' do
+  describe "Needs a key" do
     command %w{mr config}
     its(:stdout) { is_expected.to include('Need a config key')}
+  end
+
+  describe "Set a key" do
+    command %w{mr config bob build}
+    its(:stdout) { is_expected.to eq('')}
+  end
+  describe "Reads a key" do
+    command %w{mr config test.bob}
+    file '.mrmuranorc', <<-EOH
+    [test]
+    bob=build
+    EOH
+    its(:stdout) { is_expected.to include('build')}
+  end
 end
 
 #  vim: set ai et sw=2 ts=2 :
