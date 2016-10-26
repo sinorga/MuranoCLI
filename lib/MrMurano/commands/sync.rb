fa93218ca3a8a1bc6587c8be7df4ad6d34ab7f92
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-10-26 08:02:24 -0500

    Convert sync{up,down} into new sync system

--- lib/MrMurano/commands/sync.rb
+++ lib/MrMurano/commands/sync.rb
@@ -3,12 +3,11 @@ command :syncdown do |c|
   c.syntax = %{mr syncdown [options]}
   c.description = %{Sync project down from Murano}
   c.option '--all', 'Sync everything'
-  c.option '-s','--[no-]files', %{Sync Static Files}
-  c.option '-a','--[no-]endpoints', %{Sync Endpoints}
-  c.option '-m','--[no-]modules', %{Sync Modules}
-  c.option '-e','--[no-]eventhandlers', %{Sync Event Handlers}
-  c.option '--roles', %{Sync Roles}
-  c.option '--users', %{Sync Users}
+
+  # Load options to control which things to sync
+  MrMurano::SyncRoot.each_option do |s,l,d|
+    c.option s, l, d
+  end
 
   c.option '--[no-]delete', %{Don't delete things from server}
   c.option '--[no-]create', %{Don't create things on server}
@@ -17,41 +16,14 @@ command :syncdown do |c|
   c.example %{Make local be like what is on the server}, %{mr syncdown --all}
   c.example %{Pull down new things, but don't delete or modify anything}, %{mr syncdown --all --no-delete --no-update}
   c.example %{Only Pull new static files}, %{mr syncdown --files --no-delete --no-update}
-  
+
   c.action do |args,options|
     options.default :delete=>true, :create=>true, :update=>true
-    MrMurano.checkSAME(options)
-    
-    if options.endpoints then
-      sol = MrMurano::Endpoint.new
+
+    MrMurano::SyncRoot.each_filtered(options.__hash__) do |name, type, klass|
+      sol = klass.new
       sol.syncdown(options)
     end
-
-    if options.modules then
-      sol = MrMurano::Library.new
-      sol.syncdown(options)
-    end
-
-    if options.eventhandlers then
-      sol = MrMurano::EventHandler.new
-      sol.syncdown(options)
-    end
-
-    if options.roles then
-      sol = MrMurano::Role.new
-      sol.syncdown(options)
-    end
-
-    if options.users then
-      sol = MrMurano::User.new
-      sol.syncdown(options)
-    end
-
-    if options.files then
-      sol = MrMurano::File.new
-      sol.syncdown(options)
-    end
-
   end
 end
 alias_command :pull, :syncdown, '--no-delete'
@@ -60,12 +32,11 @@ command :syncup do |c|
   c.syntax = %{mr syncup [options]}
   c.description = %{Sync project up into Murano}
   c.option '--all', 'Sync everything'
-  c.option '-s','--[no-]files', %{Sync Static Files}
-  c.option '-a','--[no-]endpoints', %{Sync Endpoints}
-  c.option '-m','--[no-]modules', %{Sync Modules}
-  c.option '-e','--[no-]eventhandlers', %{Sync Event Handlers}
-  c.option '--roles', %{Sync Roles}
-  c.option '--users', %{Sync Users}
+
+  # Load options to control which things to sync
+  MrMurano::SyncRoot.each_option do |s,l,d|
+    c.option s, l, d
+  end
 
   c.option '--[no-]delete', %{Don't delete things from server}
   c.option '--[no-]create', %{Don't create things on server}
@@ -77,38 +48,11 @@ command :syncup do |c|
 
   c.action do |args,options|
     options.default :delete=>true, :create=>true, :update=>true
-    MrMurano.checkSAME(options)
 
-    if options.endpoints then
-      sol = MrMurano::Endpoint.new
+    MrMurano::SyncRoot.each_filtered(options.__hash__) do |name, type, klass|
+      sol = klass.new
       sol.syncup(options)
     end
-
-    if options.modules then
-      sol = MrMurano::Library.new
-      sol.syncup(options)
-    end
-
-    if options.eventhandlers then
-      sol = MrMurano::EventHandler.new
-      sol.syncup(options)
-    end
-
-    if options.roles then
-      sol = MrMurano::Role.new
-      sol.syncup(options)
-    end
-
-    if options.users then
-      sol = MrMurano::User.new
-      sol.syncup(options)
-    end
-
-    if options.files then
-      sol = MrMurano::File.new
-      sol.syncup(options)
-    end
-
   end
 end
 alias_command :push, :syncup, '--no-delete'
