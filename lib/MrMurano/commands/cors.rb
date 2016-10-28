345e4eaa00ff881627cf61931b68f8ec92d89bd0
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-10-28 08:45:14 -0500

    Depercated cors subcommand.

--- lib/MrMurano/commands/cors.rb
+++ lib/MrMurano/commands/cors.rb
@@ -1,20 +1,27 @@
+require 'yaml'
 require 'MrMurano/Solution-Cors'
 
 command :cors do |c|
   c.syntax = %{mr cors [options]}
-  c.description = %{Get or set the CORS for the solution.}
+  c.summary = %{}
+  c.description = %{Get or set the CORS for the solution.
+
+This is deprecated.  Use `mr syncup --cors` or `mr syncdown --cors` instead.
+  }
   c.option '-f','--file FILE', String, %{File to set CORS from}
 
   c.action do |args,options|
+    say_warning "This is deprecated.  Use `mr syncup --cors` or `mr syncdown --cors` instead."
     sol = MrMurano::Cors.new
 
     if options.file then
       #set
-      pp sol.upload(options.file, {})
+      data = sol.localitems(options.file)
+      pp sol.upload(options.file, data.first)
     else
       # get
       ret = sol.fetch()
-      puts ret
+      puts ret.to_yaml
     end
   end
 
