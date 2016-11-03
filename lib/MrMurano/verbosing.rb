adbdafb35a044d8aab0c952166e33f32b25477af
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-11-03 09:12:46 -0500

    method for outputting tabular data.

--- lib/MrMurano/verbosing.rb
+++ lib/MrMurano/verbosing.rb
@@ -2,6 +2,8 @@ require 'highline'
 require 'yaml'
 require 'json'
 require 'pp'
+require 'csv'
+require 'terminal-table'
 
 module MrMurano
   module Verbose
@@ -25,6 +27,36 @@ module MrMurano
       $stderr.puts HighLine.color(msg, :red)
     end
 
+    def tabularize(data, ios=nil)
+      fmt = $cfg['tool.outformat']
+      ios = $stdout if ios.nil?
+      cols = []
+      rows = [[]]
+      title = nil
+      if data.kind_of?(Hash) then
+        cols = data[:headers] if data.has_key?(:headers)
+        rows = data[:rows] if data.has_key?(:rows)
+        title = data[:title]
+      elsif data.kind_of?(Array) then
+        rows = data
+      elsif data.respond_to?(:to_a) then
+        rows = data.to_a
+      elsif data.respond_to?(:each) then
+        rows = []
+        data.each{|i| rows << i}
+      else
+        error "Don't know how to tabularize data."
+        return
+      end
+      if fmt =~ /csv/i then
+        CSV(ios, :headers=>cols, :write_headers=>(not cols.empty?)) do |csv|
+          rows.each{|v| csv << v}
+        end
+      else
+        # table.
+        ios.puts Terminal::Table.new :title=>title, :headings=>cols, :rows=>rows
+      end
+    end
 
     ## Format and print the object
     def outf(obj, ios=nil, &block)
