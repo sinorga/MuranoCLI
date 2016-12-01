a66ae8c27fc9b6f5f6d56c720caf9a4f68ce6a3d
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-12-01 10:32:33 -0600

    print more

--- spec/cmd_init_spec.rb
+++ spec/cmd_init_spec.rb
@@ -25,6 +25,7 @@ RSpec.describe 'mr init' do
   it "Won't init in HOME (gracefully)" do
     # this is in the project dir. Want to be in HOME
     Dir.chdir(ENV['HOME']) do
+      pp "#{pref}mr init --trace"
       out, err, status = Open3.capture3("#{pref}mr init --trace")
       expect(out).to eq("")
       expect(err).to eq("\e[31mCannot init a project in your HOME directory.\e[0m\n")
@@ -34,6 +35,7 @@ RSpec.describe 'mr init' do
 
   it "Asks to import if Solutionfile exists" do
     FileUtils.touch('Solutionfile.json')
+    pp "#{pref}mr init --trace"
     out, err, status = Open3.capture3("#{pref}mr init --trace", :stdin_data=>'y')
     expect(out).to eq("A Solutionfile.json exists, Do you want exit and run `mr config import` instead? [yN]\n")
     expect(err).to eq("")
