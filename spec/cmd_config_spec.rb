9939cb955299644988e1543d78298b06578cab31
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-12-01 11:11:27 -0600

    fix up test

--- spec/cmd_config_spec.rb
+++ spec/cmd_config_spec.rb
@@ -6,19 +6,18 @@ require 'tmpdir'
 
 RSpec.describe 'mr config' do
 
-  $realpwd = Pathname.new(Dir.pwd).realpath
   def capcmd(*args)
     args = [args] unless args.kind_of? Array
     args.flatten!
-    args[0] = $realpwd + 'bin' + args[0]
-    args.unshift("ruby", "-I#{($realpwd+'lib').to_s}")
+    args[0] = @testdir + 'bin' + args[0]
+    args.unshift("ruby", "-I#{(@testdir+'lib').to_s}")
     cmd = Shellwords.join(args)
     #pp cmd
     cmd
   end
 
   around(:example) do |ex|
-    @testdir = Dir.pwd
+    @testdir = Pathname.new(Dir.pwd).realpath
     Dir.mktmpdir do |hdir|
       ENV['HOME'] = hdir
       Dir.chdir(hdir) do
@@ -45,23 +44,25 @@ RSpec.describe 'mr config' do
     expect(err).to eq('')
 
     afile = IO.read(File.join(@tmpdir, '.mrmuranorc'))
-    bfile = IO.read(File.join(@testdir, 'spec','fixtures','mrmuranorc_tool_bob'))
+    bfile = (@testdir + 'spec' + 'fixtures' + 'mrmuranorc_tool_bob').read
     expect(afile).to eq(bfile)
   end
 
   it "Sets a user key" do
-    out, err, status = Open3.capture3("#{pref}mr config bob build --user")
+    out, err, status = Open3.capture3(capcmd(%w{mr config bob build --user}))
     expect(status).to eq(0)
     expect(out).to eq('')
     expect(err).to eq('')
 
     afile = IO.read(File.join(ENV['HOME'], '.mrmuranorc'))
-    bfile = IO.read(File.join(@testdir, 'spec','fixtures','mrmuranorc_tool_bob'))
+    bfile = (@testdir + 'spec' + 'fixtures' + 'mrmuranorc_tool_bob').read
     expect(afile).to eq(bfile)
   end
 
   it "Reads a key" do
-    FileUtils.copy_file ($realpwd+'spec/fixtures/.mrmuranorc').to_s, File.join(@tmpdir, '.mrmuranorc'), :verbose => true
+    FileUtils.copy_file((@testdir+'spec'+'fixtures'+'.mrmuranorc').to_s,
+                        File.join(@tmpdir, '.mrmuranorc'),
+                        :verbose => true)
     out, err, status = Open3.capture3(capcmd(%w{mr config --project doThisTest.bob}))
     expect(status).to eq(0)
     expect(out).to eq("build\n")
@@ -70,18 +71,17 @@ RSpec.describe 'mr config' do
 
   it "Removes a key" do
     rcf = File.join(@tmpdir, '.mrmuranorc')
-    FileUtils.copy_file ($realpwd+'spec/fixtures/.mrmuranorc').to_s, rcf, :verbose => true
+    FileUtils.copy_file((@testdir+'spec'+'fixtures'+'.mrmuranorc').to_s,
+                        rcf, :verbose => true)
     out, err, status = Open3.capture3(capcmd(%w{mr config --project --unset doThisTest.bob}))
     expect(status).to eq(0)
     expect(out).to eq('')
     expect(err).to eq('')
 
     afile = IO.read(rcf)
-    bfile = IO.read(File.join(@testdir, 'spec','fixtures','mrmuranorc_deleted_bob'))
+    bfile = (@testdir + 'spec' + 'fixtures' + 'mrmuranorc_deleted_bob').read
     expect(afile).to eq(bfile)
   end
-
-
 
 end
 
