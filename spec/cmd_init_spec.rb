3d2030b14c9dad1c18b666e67bad165cb67bda91
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-12-01 10:46:25 -0600

    Deal with paths that need escaping

--- spec/cmd_init_spec.rb
+++ spec/cmd_init_spec.rb
@@ -1,12 +1,20 @@
 require 'fileutils'
 require 'open3'
 require 'pathname'
+require 'shellwords'
 require 'tmpdir'
 
 RSpec.describe 'mr init' do
 
-  pwd = Pathname.new(Dir.pwd).realpath
-  pref = "ruby -I#{(pwd+'lib').to_s} #{(pwd+'bin').to_s}/"
+  $realpwd = Pathname.new(Dir.pwd).realpath
+  def capcmd(*args)
+    args = [args] unless args.kind_of? Array
+    args[0] = $realpwd + 'bin' + args[0]
+    args.unshift("ruby", "-I#{($realpwd+'lib').to_s}")
+    cmd = Shellwords.join(args)
+    pp cmd
+    cmd
+  end
 
   around(:example) do |ex|
     @testdir = Dir.pwd
@@ -25,8 +33,7 @@ RSpec.describe 'mr init' do
   it "Won't init in HOME (gracefully)" do
     # this is in the project dir. Want to be in HOME
     Dir.chdir(ENV['HOME']) do
-      pp "#{pref}mr init --trace"
-      out, err, status = Open3.capture3("#{pref}mr init --trace")
+      out, err, status = Open3.capture3(capcmd('mr', 'init', '--trace'))
       expect(out).to eq("")
       expect(err).to eq("\e[31mCannot init a project in your HOME directory.\e[0m\n")
       expect(status.exitstatus).to eq(2)
@@ -35,8 +42,7 @@ RSpec.describe 'mr init' do
 
   it "Asks to import if Solutionfile exists" do
     FileUtils.touch('Solutionfile.json')
-    pp "#{pref}mr init --trace"
-    out, err, status = Open3.capture3("#{pref}mr init --trace", :stdin_data=>'y')
+    out, err, status = Open3.capture3(capcmd('mr', 'init', '--trace'), :stdin_data=>'y')
     expect(out).to eq("A Solutionfile.json exists, Do you want exit and run `mr config import` instead? [yN]\n")
     expect(err).to eq("")
     expect(status.exitstatus).to eq(0)
