e171cac89480f2ea983caed7c38fcdcb4d5890f8
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-12-28 13:33:18 -0600

    more coverage
    MRMUR-30

--- spec/MakePretties_spec.rb
+++ spec/MakePretties_spec.rb
@@ -45,6 +45,17 @@ RSpec.describe MrMurano::Pretties do
     expect(ret).to eq(str)
   end
 
+  it "makes it pretty; localtime" do
+    data ={:type=>"debug", :timestamp=>1476386031,
+           :subject=>"websocket_websocket_info",
+           :data=>"Script Error: "}
+    str ="\e[31m\e[48;5;231mDEBUG \e[0m\e[31m\e[48;5;231m[websocket_websocket_info]\e[0m \e[34m2016-10-13T14:13:51.000-05:00\e[0m:\nScript Error: "
+    @options[:localtime] = true
+    ret = MrMurano::Pretties::makePretty(data, @options)
+    @options[:localtime] = false
+    expect(ret).to eq(str)
+  end
+
   it "makes it pretty; missing timestamp" do
     data ={:type=>"debug",
            :subject=>"websocket_websocket_info",
@@ -76,6 +87,29 @@ RSpec.describe MrMurano::Pretties do
            :subject=>"websocket_websocket_info",
            :data=>"Script Error: "}
     str ="\e[31m\e[48;5;231mDEBUG \e[0m\e[31m\e[48;5;231m[websocket_websocket_info]\e[0m \e[34mbob\e[0m:\nScript Error: "
+    ret = MrMurano::Pretties::makePretty(data, @options)
+    expect(ret).to eq(str)
+  end
+
+  it "makes it pretty; hash data" do
+    data ={:type=>"debug", :timestamp=>1476386031,
+           :subject=>"websocket_websocket_info",
+           :data=>{
+             :random=>'junk'
+           }}
+    str ="\e[31m\e[48;5;231mDEBUG \e[0m\e[31m\e[48;5;231m[websocket_websocket_info]\e[0m \e[34m2016-10-13T19:13:51.000+00:00\e[0m:\n\e[35m{\e[0m\n  \"random\": \"junk\"\n\e[35m}\e[0m"
+    ret = MrMurano::Pretties::makePretty(data, @options)
+    expect(ret).to eq(str)
+  end
+
+  it "makes it pretty; http hash data" do
+    data ={:type=>"debug", :timestamp=>1476386031,
+           :subject=>"websocket_websocket_info",
+           :data=>{
+             :request=>{:method=>'get'},
+             :response=>{:status=>200},
+           }}
+    str =%|\e[31m\e[48;5;231mDEBUG \e[0m\e[31m\e[48;5;231m[websocket_websocket_info]\e[0m \e[34m2016-10-13T19:13:51.000+00:00\e[0m:\n---------\nrequest:\e[35m{\e[0m\n  "method\": \"get\"\n\e[35m}\e[0m\n---------\nresponse:\e[35m{\e[0m\n  \"status\": 200\n\e[35m}\e[0m|
     ret = MrMurano::Pretties::makePretty(data, @options)
     expect(ret).to eq(str)
   end
