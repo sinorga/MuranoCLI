3338f61faa39fe98376e270b45c4cc816fc8537e
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-02-08 09:13:08 -0600

    Add password commands.
    MRMUR-50
    Fix #31

---
+++ lib/MrMurano/commands/password.rb
@@ -0,0 +1,74 @@
+
+command :password do |c|
+  c.syntax = %{murano password}
+  c.summary = %{About password commands}
+  c.description = %{Sub-commands for working with usernames and passwords}
+  c.action do |args, options|
+    ::Commander::UI.enable_paging
+    say MrMurano::SubCmdGroupHelp.new(c).get_help
+  end
+end
+
+alias_command 'password current', :config, 'user.name'
+
+command 'password list' do |c|
+  c.syntax = %{murano password list}
+  c.summary = %{List the usernames with saved passwords}
+  c.description = %{List the usernames and hosts that have been saved}
+  c.action do |args, options|
+    psd = MrMurano::Passwords.new
+    psd.load
+
+    pp psd.list
+    # TODO. need outf()
+  end
+
+end
+
+command 'password set' do |c|
+  c.syntax = %{murano password set <username> [<host>]}
+  c.summary = %{Set password for username}
+
+  c.option '--password PASSWORD', String, %{The password to use}
+  c.option '--from_env', %{Use password in MURANO_PASSWORD}
+
+  c.action do |args, options|
+    psd = MrMurano::Passwords.new
+    psd.load
+
+    username = args.shift
+    host = args.shift
+    host = $cfg['net.host'] if host.nil?
+
+    if options.password then
+      pws = options.password
+    elsif options.from_env then
+      pws = ENV['MURANO_PASSWORD']
+    else
+      pws = ask("Password:  ") { |q| q.echo = "*" }
+    end
+
+    psd.set(host, username, pws)
+    psd.save
+
+  end
+end
+
+command 'password delete' do |c|
+  c.syntax = %{murano password delete <username> [<host>]}
+  c.summary = %{Delete password for username}
+
+  c.action do |args, options|
+    psd = MrMurano::Passwords.new
+    psd.load
+
+    username = args.shift
+    host = args.shift
+    host = $cfg['net.host'] if host.nil?
+
+    psd.remove(host, username)
+    psd.save
+  end
+end
+
+#  vim: set ai et sw=2 ts=2 :
