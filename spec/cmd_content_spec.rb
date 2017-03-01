8f57c954ec91f89af6fdb3e7f6c77ff2b499f3c4
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-03-01 10:45:26 -0600

    Test for content commands.

---
+++ spec/cmd_content_spec.rb
@@ -0,0 +1,79 @@
+require 'fileutils'
+require 'open3'
+require 'pathname'
+require 'cmd_common'
+
+RSpec.describe 'murano content', :cmd, :needs_password do
+  include_context "CI_CMD"
+
+  before(:example) do
+      out, err, status = Open3.capture3(capcmd('murano', 'business', 'list', '--idonly'))
+      aout = out.split.first
+      expect(aout).to match(/^[a-zA-Z0-9]+$/)
+      expect(err).to eq('')
+      expect(status.exitstatus).to eq(0)
+
+      out, err, status = Open3.capture3(capcmd('murano', 'config', 'business.id', aout))
+      expect(out).to eq('')
+      expect(err).to eq('')
+      expect(status.exitstatus).to eq(0)
+
+      out, err, status = Open3.capture3(capcmd('murano', 'product', 'create', 'contentTest', '--save'))
+      expect(out.chomp).to match(/^[a-zA-Z0-9]+$/)
+      expect(err).to eq('')
+      expect(status.exitstatus).to eq(0)
+  end
+  after(:example) do
+      out, err, status = Open3.capture3(capcmd('murano', 'product', 'delete', 'contentTest'))
+      expect(out).to eq('')
+      expect(err).to eq('')
+      expect(status.exitstatus).to eq(0)
+  end
+
+
+  it "life cycle" do
+      out, err, status = Open3.capture3(capcmd('murano', 'content', 'list'))
+      expect(out).to eq('')
+      expect(err).to eq('')
+      expect(status.exitstatus).to eq(0)
+
+      FileUtils.copy(File.join(@testdir, 'spec/fixtures/dumped_config'), 'dumped_config')
+      out, err, status = Open3.capture3(capcmd('murano', 'content', 'upload', 'myFile', 'dumped_config', '--meta', 'random junk'))
+      expect(out).to eq('')
+      expect(err).to eq('')
+      expect(status.exitstatus).to eq(0)
+
+      out, err, status = Open3.capture3(capcmd('murano', 'content', 'list'))
+      expect(out).to eq("myFile\n")
+      expect(err).to eq('')
+      expect(status.exitstatus).to eq(0)
+
+      out, err, status = Open3.capture3(capcmd('murano', 'content', 'info', 'myFile'))
+      expect(err).to eq('')
+      olines = out.lines
+      expect(olines[0]).to match(/^(\+-+){5}\+$/)
+      expect(olines[1]).to match(/^\| \S+\s+\| \d+\s+\| \d+\s+\| random junk\s+\| (false|true)\s+\|$/)
+      expect(olines[2]).to match(/^(\+-+){5}\+$/)
+      expect(status.exitstatus).to eq(0)
+
+      out, err, status = Open3.capture3(capcmd('murano', 'content', 'download', 'myFile', '-o', 'testDown'))
+      expect(out).to eq('')
+      expect(err).to eq('')
+      expect(status.exitstatus).to eq(0)
+      expect(File.exist?('testDown')).to be true
+      expect(FileUtils.cmp('dumped_config', 'testDown')).to be true
+
+      out, err, status = Open3.capture3(capcmd('murano', 'content', 'delete', 'myFile'))
+      expect(out).to eq('')
+      expect(err).to eq('')
+      expect(status.exitstatus).to eq(0)
+
+      out, err, status = Open3.capture3(capcmd('murano', 'content', 'list'))
+      expect(out).to eq('')
+      expect(err).to eq('')
+      expect(status.exitstatus).to eq(0)
+  end
+
+end
+
+#  vim: set ai et sw=2 ts=2 :
