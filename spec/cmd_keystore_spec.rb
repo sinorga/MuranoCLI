e4058a9f12a49f61db116cf1f33da7027042f8fe
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-03-02 08:27:19 -0600

    change name of test solution

--- spec/cmd_keystore_spec.rb
+++ spec/cmd_keystore_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'murano keystore', :cmd, :needs_password do
   include_context "CI_CMD"
 
   before(:example) do
-    out, err, status = Open3.capture3(capcmd('murano', 'solution', 'create', 'corstest', '--save'))
+    out, err, status = Open3.capture3(capcmd('murano', 'solution', 'create', 'keystoretest', '--save'))
     expect(out.chomp).to match(/^[a-zA-Z0-9]+$/)
     expect(err).to eq('')
     expect(status.exitstatus).to eq(0)
@@ -19,7 +19,7 @@ RSpec.describe 'murano keystore', :cmd, :needs_password do
     expect(status.exitstatus).to eq(0)
   end
   after(:example) do
-    out, err, status = Open3.capture3(capcmd('murano', 'solution', 'delete', 'corstest'))
+    out, err, status = Open3.capture3(capcmd('murano', 'solution', 'delete', 'keystoretest'))
     expect(out).to eq('')
     expect(err).to eq('')
     expect(status.exitstatus).to eq(0)
