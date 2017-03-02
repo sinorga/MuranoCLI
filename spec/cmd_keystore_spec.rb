089716ad919b564705899eb3904036485af4c0fb
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-03-02 07:58:57 -0600

    Initial keystore command tests.

---
+++ spec/cmd_keystore_spec.rb
@@ -0,0 +1,85 @@
+require 'fileutils'
+require 'open3'
+require 'pathname'
+require 'json'
+require 'cmd_common'
+
+RSpec.describe 'murano keystore', :cmd, :needs_password do
+  include_context "CI_CMD"
+
+  before(:example) do
+    out, err, status = Open3.capture3(capcmd('murano', 'solution', 'create', 'corstest', '--save'))
+    expect(out.chomp).to match(/^[a-zA-Z0-9]+$/)
+    expect(err).to eq('')
+    expect(status.exitstatus).to eq(0)
+
+    out, err, status = Open3.capture3(capcmd('murano', 'keystore', 'set', 'bob', 'built'))
+    expect(out.chomp).to eq('')
+    expect(err).to eq('')
+    expect(status.exitstatus).to eq(0)
+  end
+  after(:example) do
+    out, err, status = Open3.capture3(capcmd('murano', 'solution', 'delete', 'corstest'))
+    expect(out).to eq('')
+    expect(err).to eq('')
+    expect(status.exitstatus).to eq(0)
+  end
+
+  it "gets" do
+    out, err, status = Open3.capture3(capcmd('murano', 'keystore', 'get', 'bob'))
+    expect(out.chomp).to eq('built')
+    expect(err).to eq('')
+    expect(status.exitstatus).to eq(0)
+  end
+
+  it "lists" do
+    out, err, status = Open3.capture3(capcmd('murano', 'keystore', 'list'))
+    expect(out.chomp).to eq('bob')
+    expect(err).to eq('')
+    expect(status.exitstatus).to eq(0)
+  end
+
+  it "infos" do
+    out, err, status = Open3.capture3(capcmd('murano', 'keystore', 'info', '-c', 'outformat=json'))
+    expect{out = JSON.parse(out)}.to_not raise_error
+    expect(err).to eq('')
+    expect(status.exitstatus).to eq(0)
+    expect(out).to match(
+      'quota'=>{'keys' => an_instance_of(Fixnum)},
+      'usage'=>{'keys' => 1, 'size' => an_instance_of(Fixnum)},
+    )
+  end
+
+  it "deletes" do
+    out, err, status = Open3.capture3(capcmd('murano', 'keystore', 'delete', 'bob'))
+    expect(out.chomp).to eq('')
+    expect(err).to eq('')
+    expect(status.exitstatus).to eq(0)
+
+    out, err, status = Open3.capture3(capcmd('murano', 'keystore', 'list'))
+    expect(out.chomp).to eq('')
+    expect(err).to eq('')
+    expect(status.exitstatus).to eq(0)
+  end
+
+  it "clearAll" do
+    out, err, status = Open3.capture3(capcmd('murano', 'keystore', 'set', 'another', 'value'))
+    expect(out.chomp).to eq('')
+    expect(err).to eq('')
+    expect(status.exitstatus).to eq(0)
+
+    out, err, status = Open3.capture3(capcmd('murano', 'keystore', 'clearAll'))
+    expect(out.chomp).to eq('')
+    expect(err).to eq('')
+    expect(status.exitstatus).to eq(0)
+
+    out, err, status = Open3.capture3(capcmd('murano', 'keystore', 'list'))
+    expect(out.chomp).to eq('')
+    expect(err).to eq('')
+    expect(status.exitstatus).to eq(0)
+  end
+
+
+end
+
+#  vim: set ai et sw=2 ts=2 :
