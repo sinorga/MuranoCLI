e93bb473a7c005e77d37caf897c4fb93e1249e42
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-03-03 12:53:28 -0600

    test status with ProjectFile.

--- spec/cmd_status_spec.rb
+++ spec/cmd_status_spec.rb
@@ -92,9 +92,44 @@ RSpec.describe 'murano status', :cmd, :needs_password do
     end
   end
 
+  context "with ProjectFile" do
+    before(:example) do
+      FileUtils.cp_r(File.join(@testdir, 'spec/fixtures/syncable_content/.'), '.')
+      FileUtils.move('assets','files')
+      FileUtils.mkpath('specs')
+      FileUtils.copy(File.join(@testdir, 'spec/fixtures/product_spec_files/lightbulb.yaml'), 'specs/resources.yaml')
+      FileUtils.copy(File.join(@testdir, 'spec/fixtures/ProjectFiles/only_meta.yaml'), 'test.murano')
+    end
 
+    it "status" do
+      out, err, status = Open3.capture3(capcmd('murano', 'status'))
+      expect(err).to eq('')
+      olines = out.lines
+      expect(olines[0]).to eq("Adding:\n")
+      expect(olines[1..8]).to include(
+        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua/),
+        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:4/),
+        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:7/),
+        a_string_matching(/ \+ A  .*routes\/singleRoute\.lua/),
+        a_string_matching(/ \+ S  .*files\/icon\.png/),
+        a_string_matching(/ \+ S  .*files\/index\.html/),
+        a_string_matching(/ \+ S  .*files\/js\/script\.js/),
+        a_string_matching(/ \+ M  .*modules\/table_util\.lua/),
+      )
+      expect(olines[9]).to eq("Deleteing:\n")
+      expect(olines[10..11]).to include(
+        " - M  my_library\n",
+        " - E  user_account\n",
+      )
+      expect(olines[12]).to eq("Changing:\n")
+      expect(olines[13..14]).to include(
+        a_string_matching(/ M E  .*services\/devdata\.lua/),
+        a_string_matching(/ M E  .*services\/timers\.lua/),
+      )
+      expect(status.exitstatus).to eq(0)
+    end
+  end
 
-  # TODO: With ProjectFile
   # TODO: With Solutionfile 0.2.0
   # TODO: With Solutionfile 0.3.0
 end
