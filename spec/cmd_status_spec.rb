cd0580f44928af811d3ab0cddcf3de63f683f6a4
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-03-03 12:34:30 -0600

    test filtering

--- spec/cmd_status_spec.rb
+++ spec/cmd_status_spec.rb
@@ -68,6 +68,28 @@ RSpec.describe 'murano status', :cmd, :needs_password do
       )
       expect(status.exitstatus).to eq(0)
     end
+
+    it "matches file path" do
+      out, err, status = Open3.capture3(capcmd('murano', 'status', '**/icon.png'))
+      expect(err).to eq('')
+      olines = out.lines
+      expect(olines[0]).to eq("Adding:\n")
+      expect(olines[1]).to a_string_matching(/ \+ S  .*files\/icon\.png/)
+      expect(olines[2]).to eq("Deleteing:\n")
+      expect(olines[3]).to eq("Changing:\n")
+      expect(status.exitstatus).to eq(0)
+    end
+
+    it "matches route" do
+      out, err, status = Open3.capture3(capcmd('murano', 'status', '#put#'))
+      expect(err).to eq('')
+      olines = out.lines
+      expect(olines[0]).to eq("Adding:\n")
+      expect(olines[1]).to a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:4/)
+      expect(olines[2]).to eq("Deleteing:\n")
+      expect(olines[3]).to eq("Changing:\n")
+      expect(status.exitstatus).to eq(0)
+    end
   end
 
 
