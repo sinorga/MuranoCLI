9881172a983c9502c79f6390d0e738ec5157d4a8
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-03-08 14:52:16 -0600

    there are only projects now

--- spec/cmd_cors_spec.rb
+++ spec/cmd_cors_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe 'murano cors', :cmd, :needs_password do
 
   before(:example) do
     @project_name = rname('corstest')
-    out, err, status = Open3.capture3(capcmd('murano', 'solution', 'create', @project_name, '--save'))
+    out, err, status = Open3.capture3(capcmd('murano', 'project', 'create', @project_name, '--save'))
     expect(err).to eq('')
     expect(out.chomp).to match(/^[a-zA-Z0-9]+$/)
     expect(status.exitstatus).to eq(0)
   end
   after(:example) do
-    out, err, status = Open3.capture3(capcmd('murano', 'solution', 'delete', @project_name))
+    out, err, status = Open3.capture3(capcmd('murano', 'project', 'delete', @project_name))
     expect(out).to eq('')
     expect(err).to eq('')
     expect(status.exitstatus).to eq(0)
