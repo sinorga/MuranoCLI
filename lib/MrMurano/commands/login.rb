6dbdc8852b02d02beeeb56eccb7542c9ea146ed0
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-03-15 10:31:43 -0500

    Add login sub-command.
    MRMUR-77

---
+++ lib/MrMurano/commands/login.rb
@@ -0,0 +1,20 @@
+require 'MrMurano/Account'
+
+command 'login' do |c|
+  c.syntax = %{murano login}
+  c.summary = %{Log into Murano}
+  c.description = %{Log into Murano
+
+If you are having trouble logging in, try deleting the saved password first.
+`murano password delete <username>`
+  }
+  c.option '--show-token', %{Shows the API token}
+
+  c.action do |args, options|
+    acc = MrMurano::Account.new
+    ret = acc.token
+    say ret if options.show_token
+  end
+end
+
+#  vim: set ai et sw=2 ts=2 :
