200a8382514b3b2e47e2e5a5c1187eee0d9cd641
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-04-04 08:02:53 -0500

    Patch around TCP_NODELAY bug.
    There is a bug were not having TCP_NODELAY set causes connection issues
    with Murano.  While ultimatily the bug is up there, we need to work
    around it down here.  As for Ruby, setting TCP_NODELAY was added in 2.1.
    But since the default version installed on macos is 2.0.0 we hit it.
    
    So, if the current version of Ruby is 2.0.0, then use this bit of code I
    copied from Ruby 2.1.

--- lib/MrMurano/http.rb
+++ lib/MrMurano/http.rb
@@ -166,4 +166,88 @@ module MrMurano
 
   end
 end
+
+# There is a bug were not having TCP_NODELAY set causes connection issues with
+# Murano.  While ultimatily the bug is up there, we need to work around it down
+# here.  As for Ruby, setting TCP_NODELAY was added in 2.1.  But since the default
+# version installed on macos is 2.0.0 we hit it.
+#
+# So, if the current version of Ruby is 2.0.0, then use this bit of code I copied
+# from Ruby 2.1.
+
+if RUBY_VERSION == '2.0.0' then
+  module Net
+    class HTTP < Protocol
+      def connect
+        if proxy? then
+          conn_address = proxy_address
+          conn_port    = proxy_port
+        else
+          conn_address = address
+          conn_port    = port
+        end
+
+        D "opening connection to #{conn_address}:#{conn_port}..."
+        s = Timeout.timeout(@open_timeout, Net::OpenTimeout) {
+          TCPSocket.open(conn_address, conn_port, @local_host, @local_port)
+        }
+        s.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
+        D "opened"
+        if use_ssl?
+          ssl_parameters = Hash.new
+          iv_list = instance_variables
+          SSL_IVNAMES.each_with_index do |ivname, i|
+            if iv_list.include?(ivname) and
+                value = instance_variable_get(ivname)
+              ssl_parameters[SSL_ATTRIBUTES[i]] = value if value
+            end
+          end
+          @ssl_context = OpenSSL::SSL::SSLContext.new
+          @ssl_context.set_params(ssl_parameters)
+          D "starting SSL for #{conn_address}:#{conn_port}..."
+          s = OpenSSL::SSL::SSLSocket.new(s, @ssl_context)
+          s.sync_close = true
+          D "SSL established"
+        end
+        @socket = BufferedIO.new(s)
+        @socket.read_timeout = @read_timeout
+        @socket.continue_timeout = @continue_timeout
+        @socket.debug_output = @debug_output
+        if use_ssl?
+          begin
+            if proxy?
+              buf = "CONNECT #{@address}:#{@port} HTTP/#{HTTPVersion}\r\n"
+              buf << "Host: #{@address}:#{@port}\r\n"
+              if proxy_user
+                credential = ["#{proxy_user}:#{proxy_pass}"].pack('m')
+                credential.delete!("\r\n")
+                buf << "Proxy-Authorization: Basic #{credential}\r\n"
+              end
+              buf << "\r\n"
+              @socket.write(buf)
+              HTTPResponse.read_new(@socket).value
+            end
+            # Server Name Indication (SNI) RFC 3546
+            s.hostname = @address if s.respond_to? :hostname=
+            if @ssl_session and
+                Time.now < @ssl_session.time + @ssl_session.timeout
+              s.session = @ssl_session if @ssl_session
+            end
+            Timeout.timeout(@open_timeout, Net::OpenTimeout) { s.connect }
+            if @ssl_context.verify_mode != OpenSSL::SSL::VERIFY_NONE
+              s.post_connection_check(@address)
+            end
+            @ssl_session = s.session
+          rescue => exception
+            D "Conn close because of connect error #{exception}"
+            @socket.close if @socket and not @socket.closed?
+            raise exception
+          end
+        end
+        on_connect
+      end
+    end
+  end
+end
+
 #  vim: set ai et sw=2 ts=2 :
