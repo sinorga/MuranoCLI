3e88fbadb1ae97ea30aa29182f48afdad1a2b03a
Landon Bouma <landon.bouma@gmail.com>
2017-05-12 16:56:49 -0500

    Per API changes (MUR-1984), add type=<event-type> to device2 event payload and change endpoint from <solution_id>_device2_<event_type> to <solution_id>_device2_event.

--- lib/MrMurano/Solution-Services.rb
+++ lib/MrMurano/Solution-Services.rb
@@ -222,6 +222,8 @@ module MrMurano
       attr_accessor :service
       # @return [String] Which event triggers this script
       attr_accessor :event
+      # @return [String] For device2 events, the type of event
+      attr_accessor :type
     end
 
     def initialize
@@ -294,9 +296,18 @@ module MrMurano
       path.readlines().each do |line|
         md = @match_header.match(line)
         if not md.nil? then
+          # [lb] asks: Is this too hacky?
+          if md[:service] == 'device2' then
+            event_event = 'event'
+            event_type = md[:event]
+          else
+            event_event = md[:event]
+            event_type = nil
+          end
           # header line.
           cur = EventHandlerItem.new(:service=>md[:service],
-                                     :event=>md[:event],
+                                     :event=>event_event,
+                                     :type=>event_type,
                                      :local_path=>path,
                                      :line=>lineno,
                                      :script=>line)
@@ -318,6 +329,7 @@ module MrMurano
           warning "Please add the header \"--#EVENT #{service} #{event}\""
           cur = EventHandlerItem.new(:service=>service,
                                      :event=>event,
+                                     :type=>nil,
                                      :local_path=>path,
                                      :line=>0,
                                      :line_end => lineno,
