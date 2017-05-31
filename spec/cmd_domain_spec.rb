30fb5e75011fb2ce5bc0f714918996b20570003b
Landon Bouma <landon.bouma@gmail.com>
2017-05-31 18:29:55 -0500

    Per labeling changes to Murano, change `murano project create` to `murano product create`. Also @project_name => @product_name.

--- spec/cmd_domain_spec.rb
+++ spec/cmd_domain_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe 'murano domain', :cmd, :needs_password do
   include_context "CI_CMD"
 
   before(:example) do
-    @project_name = rname('domainTest')
-    out, err, status = Open3.capture3(capcmd('murano', 'project', 'create', @project_name, '--save'))
+    @product_name = rname('domainTest')
+    out, err, status = Open3.capture3(capcmd('murano', 'product', 'create', @product_name, '--save'))
     expect(err).to eq('')
     expect(out.chomp).to match(/^[a-zA-Z0-9]+$/)
     expect(status.exitstatus).to eq(0)
