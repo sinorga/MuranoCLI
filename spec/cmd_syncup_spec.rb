7dcef3373aab4942b5f8a4265ab8d0976f35b369
Landon Bouma <landon.bouma@gmail.com>
2017-06-06 08:24:40 -0500

    Add :force => true to FileUtils.rm of symlink in rspec file.

--- spec/cmd_syncup_spec.rb
+++ spec/cmd_syncup_spec.rb
@@ -43,9 +43,12 @@ RSpec.describe 'murano syncup', :cmd, :needs_password do
       FileUtils.move('assets','files')
       FileUtils.mkpath('specs')
       FileUtils.copy(File.join(@testdir, 'spec/fixtures/product_spec_files/lightbulb.yaml'), 'specs/resources.yaml')
-      dev_symlink = File.join(Dir.tmpdir(), "murcli-test")
-      FileUtils.rm(dev_symlink)
-      FileUtils.ln_s(Dir.pwd, dev_symlink)
+      @dev_symlink = File.join(Dir.tmpdir(), "murcli-test")
+      FileUtils.rm(@dev_symlink, :force => true)
+      FileUtils.ln_s(Dir.pwd, @dev_symlink)
+    end
+    after(:example) do
+      FileUtils.rm(@dev_symlink, :force => true)
     end
 
     it "syncup" do
