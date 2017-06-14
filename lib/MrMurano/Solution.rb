c0c65e271b3271c8784094aade16861f0c954fa8
Landon Bouma <landon.bouma@gmail.com>
2017-06-14 16:51:39 -0500

    Make Solution a non-instantiable midclass.

--- lib/MrMurano/Solution.rb
+++ lib/MrMurano/Solution.rb
@@ -93,7 +93,7 @@ module MrMurano
   class Solution < SolutionBase
     def initialize
       if !defined?(@solntype) or @solntype.nil?
-        raise MrMurano::ConfigError.new("Solution subclass must set @solntype")
+        raise "Solution subclass must set @solntype"
       end
       super
     end
