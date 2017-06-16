fee37b74485cccaa0f38e8a264e7e19b80673372
Landon Bouma <landon.bouma@gmail.com>
2017-06-16 16:32:36 -0500

    Move require-solution.rb first in commands.rb; add comment about completion.rb.

--- lib/MrMurano/commands.rb
+++ lib/MrMurano/commands.rb
@@ -1,5 +1,10 @@
-require 'MrMurano/commands/businessList'
+# Require solution.rb first so util fcns. are available to all.
+require 'MrMurano/commands/solution'
+
+# DEVs: If you want to generate a completion file, uncomment this.
 #require 'MrMurano/commands/completion'
+
+require 'MrMurano/commands/businessList'
 require 'MrMurano/commands/config'
 require 'MrMurano/commands/content'
 require 'MrMurano/commands/cors'
@@ -15,10 +20,10 @@ require 'MrMurano/commands/postgresql'
 require 'MrMurano/commands/password'
 require 'MrMurano/commands/settings'
 require 'MrMurano/commands/show'
-require 'MrMurano/commands/solution'
 require 'MrMurano/commands/status'
 require 'MrMurano/commands/sync'
 require 'MrMurano/commands/tsdb'
 require 'MrMurano/commands/usage'
 
 #  vim: set ai et sw=2 ts=2 :
+
