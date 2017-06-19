81359913b8cdab62658503dd9934417ddbc73f99
Landon Bouma <landon.bouma@gmail.com>
2017-06-19 17:57:39 -0500

    Fix cmd_syncup_spec.rb to match changes I made last week; also move symlink feature code to base CI_CMD wrapper.

--- spec/cmd_syncup_spec.rb
+++ spec/cmd_syncup_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe 'murano syncup', :cmd, :needs_password do
   context "without ProjectFile" do
     before(:example) do
       FileUtils.cp_r(File.join(@testdir, 'spec/fixtures/syncable_content/.'), '.')
-      FileUtils.move('assets','files')
+      FileUtils.move('assets', 'files')
       FileUtils.mkpath('specs')
       FileUtils.copy(File.join(@testdir, 'spec/fixtures/product_spec_files/lightbulb.yaml'), 'specs/resources.yaml')
     end
@@ -52,14 +52,13 @@ RSpec.describe 'murano syncup', :cmd, :needs_password do
       expect(status.exitstatus).to eq(0)
 
       out, err, status = Open3.capture3(capcmd('murano', 'status'))
-      expect(out).to start_with(%{Adding:\nDeleting:\nChanging:\n})
+      #expect(out).to start_with(%{Adding:\nDeleting:\nChanging:\n})
+      expect(out).to start_with(%{Nothing to add\nNothing to delete\nNothing to change\n})
       # Due to timestamp races, there might be modules or services in Changing.
       expect(err).to eq('')
       expect(status.exitstatus).to eq(0)
     end
   end
-
-
 
   # TODO: With ProjectFile
   # TODO: With Solutionfile 0.2.0
