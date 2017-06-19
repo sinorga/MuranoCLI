312726fd36c70ad85fdc3312d184255ce5b4bd50
Landon Bouma <landon.bouma@gmail.com>
2017-06-19 17:54:30 -0500

    Add helpful DEV symlink feature (/tmp/murcli-test) to CI_CMD wrapper so all tests create it.

--- spec/cmd_common.rb
+++ spec/cmd_common.rb
@@ -49,6 +49,18 @@ RSpec.shared_context "CI_CMD" do
     "#{name.downcase}#{Random.new.rand.hash.abs.to_s(16)}"
   end
 
+  def mk_symlink
+    # Make it easy to debug tests, e.g., add breakpoint before capcmd('murano', ...)
+    # run test, then open another terminal window and `cd /tmp/murcli-test`.
+    @dev_symlink = File.join(Dir.tmpdir(), "murcli-test")
+    FileUtils.rm(@dev_symlink, :force => true)
+    FileUtils.ln_s(Dir.pwd, @dev_symlink)
+  end
+
+  def rm_symlink
+    FileUtils.rm(@dev_symlink, :force => true)
+  end
+
   before(:example) do
     $cfg = MrMurano::Config.new
     $cfg.load
@@ -63,9 +75,11 @@ RSpec.shared_context "CI_CMD" do
         @tmpdir = File.join(hdir, 'project')
         Dir.mkdir(@tmpdir)
         Dir.chdir(@tmpdir) do
+          mk_symlink
           Timeout::timeout(300) do
             ex.run
           end
+          rm_symlink
         end
       end
     end
