aff903b7fe3b2d85925e673804a09811aff386c9
Landon Bouma <landon.bouma@gmail.com>
2017-06-19 17:57:39 -0500

    Fix cmd_syncup_spec.rb to match changes I made last week; also move symlink feature code to base CI_CMD wrapper.

--- spec/cmd_syncup_spec.rb
+++ spec/cmd_syncup_spec.rb
@@ -40,15 +40,9 @@ RSpec.describe 'murano syncup', :cmd, :needs_password do
   context "without ProjectFile" do
     before(:example) do
       FileUtils.cp_r(File.join(@testdir, 'spec/fixtures/syncable_content/.'), '.')
-      FileUtils.move('assets','files')
+      FileUtils.move('assets', 'files')
       FileUtils.mkpath('specs')
       FileUtils.copy(File.join(@testdir, 'spec/fixtures/product_spec_files/lightbulb.yaml'), 'specs/resources.yaml')
-      @dev_symlink = File.join(Dir.tmpdir(), "murcli-test")
-      FileUtils.rm(@dev_symlink, :force => true)
-      FileUtils.ln_s(Dir.pwd, @dev_symlink)
-    end
-    after(:example) do
-      FileUtils.rm(@dev_symlink, :force => true)
     end
 
     it "syncup" do
@@ -65,14 +59,13 @@ RSpec.describe 'murano syncup', :cmd, :needs_password do
       #    M E  services/devdata.lua
       #    M E  services/timers.lua
       out, err, status = Open3.capture3(capcmd('murano', 'status'))
-      expect(out).to start_with(%{Adding:\nDeleting:\nChanging:\n})
+      #expect(out).to start_with(%{Adding:\nDeleting:\nChanging:\n})
+      expect(out).to start_with(%{Nothing to add\nNothing to delete\nNothing to change\n})
       # Due to timestamp races, there might be modules or services in Changing.
       expect(err).to eq('')
       expect(status.exitstatus).to eq(0)
     end
   end
-
-
 
   # TODO: With ProjectFile
   # TODO: With Solutionfile 0.2.0
