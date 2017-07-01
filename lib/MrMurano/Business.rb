9e66f905bc50fbf6a84034e2afd2f664d558220f
landonb <landonb@users.noreply.github.com>
2017-07-01 17:19:02 -0500

    Move Password and Business code out of Account to their own files; move global_option calls to their own file. Enhance Business class.
    - Add global options for specifying the business, product, and application,
      using names, IDs, or search terms.
      - E.g., you can now do `murano init --application myAppName --product myProdName`
    - NEW: Add copyright and license comments to the tops of files.
      - And move the vim modeline to the header comments, to be among their ilk.
    - NEW: Enforce consistent code style using Rubocop (with lots of cop
      adjustments), since the lack in style conformity was distracting and
      getting in the way of coding.
    - Store metadata about a business in the Business class object.
      It used to be that commands would process the results of business.list(),
      but it makes things easier (and reduces code duplication) if the business
      object just manages its data itself.
    - Move code for creating solutions from the command classes into the
      Business class.
      - Display specific message when 409/Conflict received, and let the
        user know their Murano account limit has been reached.

---
+++ lib/MrMurano/Business.rb
@@ -0,0 +1,313 @@
+# Last Modified: 2017.07.01 /coding: utf-8
+# frozen_string_literal: true
+
+# Copyright © 2016-2017 Exosite LLC.
+# License: MIT. See LICENSE.txt.
+#  vim:tw=0:ts=2:sw=2:et:ai
+
+require 'date'
+require 'inflecto'
+require 'json'
+require 'net/http'
+require 'pathname'
+require 'rainbow'
+require 'uri'
+require 'yaml'
+require 'MrMurano/http'
+require 'MrMurano/verbosing'
+require 'MrMurano/Config'
+require 'MrMurano/Solution'
+require 'MrMurano/Passwords'
+
+module MrMurano
+  # The Business class represents an end user's solutions.
+  class Business
+    include Http
+    include Verbose
+
+    attr_accessor :bid
+    attr_accessor :name
+
+    def initialize(data=nil)
+      if data.nil?
+        @bid = $cfg['business.id'].to_s
+        @name = $cfg['business.name'].to_s
+      else
+        self.meta = data
+      end
+      @valid = false
+      @user_bizes = {}
+    end
+
+    def valid?
+      @valid
+    end
+
+    # Consume data returned from Account::businesses.
+    def meta=(data)
+      @valid = !data.nil?
+      return unless @valid
+      @bid = data[:bizid]
+      @name = data[:name]
+    end
+
+    def write(scope=:project)
+      $cfg.set('business.id', @bid, scope)
+      $cfg.set('business.name', @name, scope)
+      self
+    end
+
+    # MAYBE: Check that ADC is enabled on the business. If not, tell
+    #   user to run Murano 2.x. [lb] is not sure which value from
+    #   Murano to check. Is it :enableMurano or :enableProjects?
+    #   See the overview method.
+    #def adc_compat_check
+    #  unless $cfg['business.id'].nil?
+    #    unless projects?($cfg['business.id'])
+    #      # This is 3.x which does not support projects!
+    #      warning('!'*80)
+    #      warning "Your business requires Murano CLI 2.x"
+    #      warning "Some features may not work correctly."
+    #      warning('!'*80)
+    #    end
+    #  end
+    #end
+
+    def must_business_id!
+      raise MrMurano::ConfigError.new(missing_business_id_msg) if @bid.nil?
+    end
+
+# FIXME: Do I need to escape the backticks?
+    def self.missing_business_id_msg
+      %(
+Missing Business ID.
+Call `#{MrMurano::EXE_NAME} business list` to get a list of business IDs.
+Set the ID temporarily using --config business.id=<ID>
+or add to the project config using \`#{MrMurano::EXE_NAME} config business.id <ID>\`
+or add to the user config using \`#{MrMurano::EXE_NAME} config business.id <ID> --user\`
+      ).strip
+    end
+
+    def pretty_name_and_id
+      "‘#{Rainbow(@name).underline}’ <#{@bid}>"
+    end
+
+    # ---------------------------------------------------------------------
+
+    #def projects?(id)
+    #  ret = get("business/#{id}/overview")
+    #  return false unless ret.is_a?(Hash)
+    #  return false unless ret.key?(:tier)
+    #  tier = ret[:tier]
+    #  return false unless tier.is_a?(Hash)
+    #  return false unless tier.key?(:enableProjects)
+    #  tier[:enableProjects]
+    #end
+
+    def overview(&block)
+      # Here are all the goodies that the overview endpoint returns:
+      #  {:name=>"XXX", :email=>"XXX", :contact=>"XXX",
+      #   :billing=>{
+      #     :terms=>0, :balance=>0, :overdue=>0},
+      #   :tier=>{
+      #     :name=>"Community", :id=>"free", :price=>0, :users=>nil, :domains=>0,
+      #     :ssl=>false, :rebrand=>false, :billing=>false, :multisolution=>true,
+      #     :phonesupport=>false, :supportLevel=>"Community", :servicesTier=>"Core",
+      #     :onboarding=>{}, :enableMurano=>true, :enableProjects=>true},
+      #   :accountManager=>{
+      #     :name=>"", :email=>"", :phone=>""},
+      #   :accountLimits=>{
+      #     :teamMembers=>1, :solutionApis=>1, :productModels=>1,
+      #     :perModelDevices=>10, :perApiUsers=>10}, :lineitems=>[]}
+      # EXPLAIN/2017-06-30: Which value(s) tell us if ADC is enabled?
+      whirly_start('Fetching Business...')
+      data = get("business/#{@bid}/overview", &block)
+      whirly_stop
+      @valid = !data.nil?
+      @name = data[:name] if @valid
+      @valid
+    end
+
+    # ---------------------------------------------------------------------
+
+    # 2017-06-30: In ADC-enabled Murano, there are now just 2 solution types.
+    # LATER: This'll change in the future; Murano will support arbitrary
+    #   solution types.
+    #ALLOWED_TYPES = [:domain, :onepApi, :dataApi, :application, :product,].freeze
+    ALLOWED_TYPES = %i[application product].freeze
+
+    def solutions(type=:all, invalidate=false, match_sid=nil, match_name=nil, match_either=nil)
+      debug "Getting all solutions of type #{type}"
+      must_business_id!
+
+      type = type.to_sym
+      raise "Unknown type(#{type})" unless type == :all || ALLOWED_TYPES.include?(type)
+      # Cache the result since sometimes both products() and applications() are called.
+      if invalidate || @user_bizes[type].nil?
+        got = get('business/' + @bid + '/solution/')
+        @user_bizes[type] = got
+      end
+      solz = @user_bizes[type].dup
+      solz.select! { |i| i[:type] == type.to_s } unless type == :all
+
+      solz.select! { |sol| sol[:apiId] == match_sid } unless match_sid.to_s.empty?
+      solz.select! { |sol| sol[:name] == match_name } unless match_name.to_s.empty?
+      unless match_either.to_s.empty?
+        solz.select! do |sol|
+          sol[:name] == match_either || sol[:apiId] == match_either
+        end
+      end
+
+      solz.map do |meta|
+        case meta[:type].to_sym
+        when :application
+          MrMurano::Application.new(meta)
+        when :product
+          MrMurano::Product.new(meta)
+        else
+          warning("Unexpected solution type: #{meta[:type]}")
+          MrMurano::Solution.new(meta)
+        end
+      end
+    end
+
+    ## Given a type (:application or :product), return a Solution instance.
+    def solution_from_type!(type)
+      type = type.to_s.to_sym
+      raise "Unknown type(#{type})" unless ALLOWED_TYPES.include? type
+      sid = MrMurano::Solution::INVALID_SID
+      if type == :application
+        sol = MrMurano::Application.new(sid)
+      elsif type == :product
+        sol = MrMurano::Product.new(sid)
+      else
+        raise "Unexpected path: Unrecognized type ‘#{type}’"
+      end
+      sol.biz = self
+      sol
+    end
+
+    ## Create a new solution in the current business
+    def new_solution!(name, type)
+      must_business_id!
+      sol = solution_from_type!(type)
+      sol.set_name!(name)
+      if $cfg['tool.dry']
+        say "--dry: Not creating solution #{name}"
+        return nil
+      end
+      whirly_start 'Creating solution...'
+      resp = post(
+        'business/' + @bid + '/solution/',
+        label: sol.name,
+        type: sol.type,
+      ) do |request, http|
+        response = http.request(request)
+        MrMurano::Verbose.whirly_stop
+        if response.is_a?(Net::HTTPSuccess)
+          workit_response(response)
+        else
+          MrMurano::Verbose.error(
+            "Unable to create #{sol.type_name}: ‘#{sol.name}’"
+          )
+          ok = false
+          if response.is_a?(Net::HTTPConflict)
+            _isj, jsn = isJSON(response.body)
+            if jsn[:message] == 'limit'
+              ok = true
+              MrMurano::Verbose.error(
+                "You've reached your limit of #{Inflecto.pluralize(sol.type.to_s)}."
+              )
+            else
+              ok = false
+            end
+          end
+          showHttpError(request, response) unless ok
+          # Hard stop.
+          exit 1
+          nil
+        end
+      end
+      whirly_stop
+      new_solution_prepare!(sol, resp)
+    end
+
+    def new_solution_prepare!(sol, resp)
+      if resp.nil?
+        error("Create #{sol.type_name} failed: Request failed")
+        exit 1
+      end
+      unless resp.is_a?(Hash)
+        error("Create #{sol.type_name} failed: Unexpected response: #{resp}")
+        exit 1
+      end
+      if resp[:id].to_s.empty?
+        error("Unexpected: Solution ID not returned: #{resp}")
+        exit 1
+      end
+      sol.sid = resp[:id]
+      # 2017-06-29: The code used to hunt for the solution ID, because
+      #   POST business/<bizid>/solution/ used to not return anything,
+      #   but now it returns the solution ID.
+      # FIXME: Delete this eventually, once you verify the new behavior.
+      #if false
+      #  # Create doesn't return anything, so go looking for it.
+      #  MrMurano::Verbose.whirly_start('Verifying solution...')
+      #  invalidate_cache = true
+      #  ret = solutions(sol.type, invalidate_cache).select do |meta|
+      #    meta[:name] == sol.name || meta[:domain] =~ /#{sol.name}\./i
+      #  end
+      #  MrMurano::Verbose.whirly_stop
+      #  if ret.count > 1
+      #    warning("Found more than 1 matching solution: #{ret}")
+      #  elsif ret.count.zero?
+      #    error("Unable to verify solution created for ‘#{sol.name}’: #{ret}")
+      #    exit 3
+      #  end
+      #  sol.meta = ret.first
+      #  if sol.sid.to_s.empty? then
+      #    error("New solution created for ‘#{sol.name}’ missing ID?: #{ret}")
+      #    exit 3
+      #  end
+      #  sol.sid = sid
+      #end
+      sol
+    end
+
+    def delete_solution(sid)
+      must_business_id!
+      delete('business/' + @bid + '/solution/' + sid)
+    end
+
+    # ---------------------------------------------------------------------
+
+    def products
+      solutions(:product)
+    end
+
+    ## Create a new product in the current business
+    def new_product(name, type=:product)
+      new_solution!(name, type)
+    end
+
+    def delete_product(sid)
+      delete_solution(sid)
+    end
+
+    # ---------------------------------------------------------------------
+
+    def applications
+      solutions(:application)
+    end
+
+    ## Create a new application in the current business
+    def new_application(name, type=:application)
+      new_solution!(name, type)
+    end
+
+    def delete_application(sid)
+      delete_solution(sid)
+    end
+  end
+end
+
---
+++ lib/MrMurano/commands/globals.rb
@@ -0,0 +1,110 @@
+# Last Modified: 2017.07.01 /coding: utf-8
+# frozen_string_literal: true
+
+# Copyright © 2016-2017 Exosite LLC.
+# License: MIT. See LICENSE.txt.
+#  vim:tw=0:ts=2:sw=2:et:ai
+
+global_option('-V', '--verbose', %(Be chatty)) do
+  $cfg['tool.verbose'] = true
+end
+
+global_option('-n', '--dry', %(Don't run actions that make changes)) do
+  $cfg['tool.dry'] = true
+  # Running dry implies verbose.
+  $cfg['tool.verbose'] = true
+end
+
+global_option('-L', '--curl', %(Print out a curl command for each network call)) do
+  $cfg['tool.curldebug'] = true
+end
+
+global_option '--skip-plugins', %(Don't load plugins. Good for when one goes bad) do
+  # no-op
+end
+
+global_option('-C', '--configfile FILE', %(Load additional configuration file)) do |file|
+  # This is called after all of the top level code in this file.
+  $cfg.load_specific(file)
+end
+
+global_option('-c', '--config KEY=VALUE', %(Set a single config key)) do |param|
+  key, value = param.split('=', 2)
+  # a=b :> ["a", "b"]
+  # a= :> ["a", ""]
+  # a :> ["a"]
+  raise "Bad config '#{param}'" if key.nil?
+  if value.nil?
+    $cfg[key] = 'true'
+  else
+    $cfg[key] = value
+  end
+end
+
+global_option('--[no-]color', %(Disable fancy output)) do |value|
+  HighLine.use_color = value
+  Rainbow.enabled = value
+end
+
+global_option('--[no-]progress', %(Disable spinner and progress message)) do |value|
+  $cfg['tool.no-progress'] = !value
+end
+
+# 2017-06-30: Being more flexible. And more consistent. Some commands
+# just use the IDs found in config, but some commands allow the user
+# to pass solution names or IDs as arguments to the command, and some
+# commands should be able to work without being run from inside a project
+# directory.
+
+# The user can indicate the business, application, and product via command line.
+# If not specified, we'll look in the config. Or, if the user is running the
+# init command, we'll interact with the user.
+
+global_option('--business BUSINESS', String, %(Name or ID of Murano Business to use)) do |business_mark|
+  $cfg['business.id'] = nil
+  $cfg['business.name'] = nil
+  $cfg['business.mark'] = business_mark
+end
+global_option('--business-name NAME', String, %(Name of Murano Business to use)) do |business_name|
+  $cfg['business.id'] = nil
+  $cfg['business.name'] = business_name
+  $cfg['business.mark'] = nil
+end
+global_option('--business-id ID', String, %(ID of Murano Business to use)) do |business_id|
+  $cfg['business.id'] = business_id
+  $cfg['business.name'] = nil
+  $cfg['business.mark'] = nil
+end
+
+global_option('--application APPLICATION', String, %(Name or ID of Application to use)) do |application_mark|
+  $cfg['application.id'] = nil
+  $cfg['application.name'] = nil
+  $cfg['application.mark'] = application_mark
+end
+global_option('--application-name NAME', String, %(Name of Application to use)) do |application_name|
+  $cfg['application.id'] = nil
+  $cfg['application.name'] = application_name
+  $cfg['application.mark'] = nil
+end
+global_option('--application-id ID', String, %(ID of Application to use)) do |application_id|
+  $cfg['application.id'] = application_id
+  $cfg['application.name'] = nil
+  $cfg['application.mark'] = nil
+end
+
+global_option('--product PRODUCT', String, %(Name or ID of Product to use)) do |product_mark|
+  $cfg['product.id'] = nil
+  $cfg['product.name'] = nil
+  $cfg['product.mark'] = product_mark
+end
+global_option('--product-name NAME', String, %(Name of Product to use)) do |product_name|
+  $cfg['product.id'] = nil
+  $cfg['product.name'] = product_name
+  $cfg['product.mark'] = nil
+end
+global_option('--product-id ID', String, %(ID of Product to use)) do |product_id|
+  $cfg['product.id'] = product_id
+  $cfg['product.name'] = nil
+  $cfg['product.mark'] = nil
+end
+
