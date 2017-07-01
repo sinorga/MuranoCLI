b9acf8e2b51078ae573a05f3271be612be92421d
landonb <landonb@users.noreply.github.com>
2017-07-01 17:19:02 -0500

    Move Password and Business code out of Account to their own files; move global_option calls to their own file. Enhance Business class.
    - Add global options for specifying the business, product, and application,
      using names, IDs, or search terms.
      - E.g., you can now do `murano init --application myAppName --product myProdName`
    - NEW: Add copyright and license comments to the tops of files.
      - And move the vim modeline to the header comments, to be among their ilk.
    - NEW: Enforce consistent code style using Rubocop (with lots of cop
      adjustments), since the lack in style conformity was distracting and
      getting in the way of coding.
    - Store metadata about a business in the Business class object.
      It used to be that commands would process the results of business.list(),
      but it makes things easier (and reduces code duplication) if the business
      object just manages its data itself.
    - Move code for creating solutions from the command classes into the
      Business class.
      - Display specific message when 409/Conflict received, and let the
        user know their Murano account limit has been reached.

---
+++ lib/MrMurano/Passwords.rb
@@ -0,0 +1,99 @@
+# Last Modified: 2017.07.01 /coding: utf-8
+# frozen_string_literal: true
+
+# Copyright © 2016-2017 Exosite LLC.
+# License: MIT. See LICENSE.txt.
+#  vim:tw=0:ts=2:sw=2:et:ai
+
+require 'date'
+require 'json'
+require 'net/http'
+require 'pathname'
+require 'uri'
+require 'yaml'
+require 'MrMurano/http'
+require 'MrMurano/verbosing'
+require 'MrMurano/Config'
+require 'MrMurano/Solution'
+
+module MrMurano
+  # The Passwords class manages an end user's Murano user name and password.
+  class Passwords
+    include Verbose
+
+    def initialize(path=nil)
+      path = $cfg.file_at('passwords', :user) if path.nil?
+      path = Pathname.new(path) unless path.is_a? Pathname
+      @path = path
+      @data = nil
+    end
+
+    def load
+      return unless @path.exist?
+      @path.chmod(0o600)
+      @path.open('rb') do |io|
+        # 2017-07-01: Rubocop suggests using safe_load.
+        #  https://ruby-doc.org/stdlib-2.1.0/libdoc/psych/rdoc/Psych.html#method-c-load
+        #  https://ruby-doc.org/stdlib-2.1.0/libdoc/psych/rdoc/Psych.html#method-c-safe_load
+        #@data = YAML.load(io)
+        @data = YAML.safe_load(io)
+      end
+    end
+
+    def save
+      if $cfg['tool.dry']
+        say '--dry: Not saving config'
+        return
+      end
+      @path.dirname.mkpath unless @path.dirname.exist?
+      @path.open('wb') do |io|
+        io << @data.to_yaml
+      end
+      @path.chmod(0o600)
+    end
+
+    def set(host, user, pass)
+      unless @data.is_a? Hash
+        @data = { host => { user => pass } }
+        return
+      end
+      hd = @data[host]
+      if hd.nil? || !hd.is_a?(Hash)
+        @data[host] = { user => pass }
+        return
+      end
+      @data[host][user] = pass
+    end
+
+    def get(host, user)
+      return ENV['MURANO_PASSWORD'] unless ENV['MURANO_PASSWORD'].nil?
+      unless ENV['MR_PASSWORD'].nil?
+        warning %(
+          Using deprecated environment variable, "MR_PASSWORD". Please rename to "MURANO_PASSWORD"
+        ).strip
+        return ENV['MR_PASSWORD']
+      end
+      return nil unless @data.is_a? Hash
+      return nil unless @data.is_a? host
+      return nil unless @data[host].is_a? Hash
+      return nil unless @data[host].is_a? user
+      @data[host][user]
+    end
+
+    ## Remove the password for a user.
+    def remove(host, user)
+      return unless @data.is_a?(Hash)
+      hd = @data[host]
+      return unless !hd.nil? && hd.is_a?(Hash)
+      @data[host].delete(user) if hd.key?(user)
+    end
+
+    ## Get all hosts and usernames. Does not return passwords.
+    def list
+      ret = {}
+      @data&.each_pair { |key, value| ret[key] = value.keys }
+      ret
+    end
+  end
+end
+
