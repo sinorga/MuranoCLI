0d6afbdb45724256f288df6c6425dfcb1d69da73
landonb <landonb@users.noreply.github.com>
2017-07-02 11:40:27 -0500

    Update domain.rb to use new Solution objects, rather than expect a Hash; also make Rubocop compliant.

--- lib/MrMurano/commands/domain.rb
+++ lib/MrMurano/commands/domain.rb
@@ -1,42 +1,49 @@
+# Last Modified: 2017.07.01 /coding: utf-8
+# frozen_string_literal: true
+
+# Copyright © 2016-2017 Exosite LLC.
+# License: MIT. See LICENSE.txt.
+#  vim:tw=0:ts=2:sw=2:et:ai
+
 require 'MrMurano/Solution'
+require 'MrMurano/commands/solution'
 
 command :domain do |c|
-  c.syntax = %{murano domain}
-  c.summary = %{Print the domain for this solution}
-  c.description = %{
+  c.syntax = %(murano domain)
+  c.summary = %(Print the domain for this solution)
+  c.description = %(
 Print the domain for this solution.
-  }.strip
-  c.option '--[no-]raw', %{Don't add scheme}
-  c.option '--[no-]brief', %{Show the URL but not the solution ID}
+  ).strip
+  c.option '--[no-]raw', %(Don't add scheme (default with brief))
+  c.option '--[no-]brief', %(Show the URL but not the solution ID)
 
   # Add the flags: --types, --ids, --names, --[no]-header.
   command_add_solution_pickers c
 
-  c.action do |args,options|
-    options.default :raw=>true
+  c.action do |args, options|
+    options.default(raw: true) if options.brief
+
+    c.verify_arg_count!(args)
 
     solz = must_fetch_solutions!(options)
 
-    domain_s = MrMurano::Verbose::pluralize?("domain", solz.length)
-    MrMurano::Verbose::whirly_start "Fetching #{domain_s}..."
+    domain_s = MrMurano::Verbose.pluralize?('domain', solz.length)
+    MrMurano::Verbose.whirly_start("Fetching #{domain_s}...")
     solz.each do |soln|
       # Get the solution info; stores the result in the Solution object.
-      desc = soln.info
+      _meta = soln.info_safe
     end
-    MrMurano::Verbose::whirly_stop
+    MrMurano::Verbose.whirly_stop
 
-    solz.each do |soln|
-      unless options.brief
-        say soln.pretty_desc(add_type=true, no_raw=!options.raw)
+    solz.each do |sol|
+      if !options.brief
+        say(sol.pretty_desc(add_type: true, raw_url: options.raw))
+      elsif options.raw
+        say(sol.domain)
       else
-        if options.raw then
-          say soln.desc[:domain]
-        else
-          say "https://#{soln.desc[:domain]}"
-        end
+        say("https://#{sol.domain}")
       end
     end
-
   end
 end
 alias_command 'domain product', 'domain', '--type', 'product'
@@ -47,6 +54,4 @@ alias_command 'domain application', 'domain', '--type', 'application'
 alias_command 'domain applications', 'domain', '--type', 'application'
 alias_command 'domain app', 'domain', '--type', 'application'
 alias_command 'domain apps', 'domain', '--type', 'application'
-
-#  vim: set ai et sw=2 ts=2 :
 
