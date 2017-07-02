30165c1ddd30730d3af291172e5dde672a616458
landonb <landonb@users.noreply.github.com>
2017-07-01 20:48:19 -0500

    Change rspec test expectation from 'solution.id' to 'application.id'.

--- spec/ConfigMigrate_spec.rb
+++ spec/ConfigMigrate_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe MrMurano::ConfigMigrate do
   it "imports all" do
     @mrt.import_secret
 
-    expect($cfg['solution.id']).to eq('ABCDEFG')
+    expect($cfg['application.id']).to eq('ABCDEFG')
     expect($cfg['product.id']).to eq('HIJKLMNOP')
     expect($cfg['user.name']).to eq('test@user.account')
     pff = $cfg.file_at('passwords', :user)
@@ -47,7 +47,7 @@ RSpec.describe MrMurano::ConfigMigrate do
   end
 
   it "imports over" do
-    $cfg['solution.id'] = '12'
+    $cfg['application.id'] = '12'
     $cfg['product.id'] = 'awdfvs'
     $cfg['user.name'] = '3qrarvsa'
     $cfg = MrMurano::Config.new
@@ -56,7 +56,7 @@ RSpec.describe MrMurano::ConfigMigrate do
 
     @mrt.import_secret
 
-    expect($cfg['solution.id']).to eq('ABCDEFG')
+    expect($cfg['application.id']).to eq('ABCDEFG')
     expect($cfg['product.id']).to eq('HIJKLMNOP')
     expect($cfg['user.name']).to eq('test@user.account')
     pff = $cfg.file_at('passwords', :user)
@@ -77,7 +77,7 @@ RSpec.describe MrMurano::ConfigMigrate do
 
     @mrt.import_secret
 
-    expect($cfg['solution.id']).to eq('ABCDEFG')
+    expect($cfg['application.id']).to eq('ABCDEFG')
     expect($cfg['product.id']).to eq('HIJKLMNOP')
     expect($cfg['user.name']).to eq('test@user.account')
     pff = $cfg.file_at('passwords', :user)
