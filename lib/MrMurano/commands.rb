2201e053fa938939f8db1e2804bf128002be1baa
landonb <landonb@users.noreply.github.com>
2017-07-02 10:51:58 -0500

    Add header to commands.rb; add new commands (solution, globals, renamed business).

--- lib/MrMurano/commands.rb
+++ lib/MrMurano/commands.rb
@@ -1,19 +1,24 @@
-# Require solution.rb first so util fcns. are available to all.
-require 'MrMurano/commands/solution'
+# Last Modified: 2017.07.02 /coding: utf-8
+# frozen_string_literal: true
+
+# Copyright © 2016-2017 Exosite LLC.
+# License: MIT. See LICENSE.txt.
+#  vim:tw=0:ts=2:sw=2:et:ai
 
 # DEVs: If you want to generate a completion file, uncomment this.
 #   Then run, e.g.,
 #     murano completion > docs/completions/murano_completion-bash
 #require 'MrMurano/commands/completion'
 
-require 'MrMurano/commands/businessList'
+require 'MrMurano/commands/business'
 require 'MrMurano/commands/config'
 require 'MrMurano/commands/content'
 require 'MrMurano/commands/cors'
 require 'MrMurano/commands/devices'
 require 'MrMurano/commands/domain'
-require 'MrMurano/commands/init'
+require 'MrMurano/commands/globals'
 require 'MrMurano/commands/keystore'
+require 'MrMurano/commands/init'
 require 'MrMurano/commands/link'
 require 'MrMurano/commands/login'
 require 'MrMurano/commands/logs'
@@ -22,10 +27,9 @@ require 'MrMurano/commands/postgresql'
 require 'MrMurano/commands/password'
 require 'MrMurano/commands/settings'
 require 'MrMurano/commands/show'
+require 'MrMurano/commands/solution'
 require 'MrMurano/commands/status'
 require 'MrMurano/commands/sync'
 require 'MrMurano/commands/tsdb'
 require 'MrMurano/commands/usage'
-
-#  vim: set ai et sw=2 ts=2 :
 
