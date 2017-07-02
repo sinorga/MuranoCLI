04ce7a64bd1f1841f4c00ba958c5d35590a96274
landonb <landonb@users.noreply.github.com>
2017-07-01 21:20:08 -0500

    Move business-related UI commands from init to businessList.rb (so to be renamed just business.rb).

--- lib/MrMurano/commands/businessList.rb
+++ lib/MrMurano/commands/businessList.rb
@@ -1,10 +1,19 @@
+# Last Modified: 2017.07.01 /coding: utf-8
+# frozen_string_literal: true
+
+# Copyright © 2016-2017 Exosite LLC.
+# License: MIT. See LICENSE.txt.
+#  vim:tw=0:ts=2:sw=2:et:ai
+
 require 'MrMurano/Account'
+require 'MrMurano/Business'
+require 'MrMurano/ReCommander'
 
 command :business do |c|
   c.syntax = %{murano business}
   c.summary = %{About business}
   c.description = %{
-Sub-commands for working with businesses.
+Commands for working with businesses.
   }.strip
   c.project_not_required = true
 
@@ -20,7 +29,7 @@ command 'business list' do |c|
   c.description = %{
 List businesses.
   }.strip
-  c.option '--idonly', 'Only return the ids'
+  c.option '--idonly', 'Only return the IDs'
   c.option '--[no-]all', 'Show all fields'
   c.option '-o', '--output FILE', %{Download to file instead of STDOUT}
   c.project_not_required = true
@@ -28,9 +37,9 @@ List businesses.
   c.action do |args, options|
     acc = MrMurano::Account.new
 
-    MrMurano::Verbose::whirly_start "Looking for businesses..."
+    MrMurano::Verbose.whirly_start 'Looking for businesses...'
     data = acc.businesses
-    MrMurano::Verbose::whirly_stop
+    MrMurano::Verbose.whirly_stop
 
     io=nil
     if options.output then
@@ -64,5 +73,80 @@ List businesses.
 end
 alias_command 'businesses list', 'business list'
 
-#  vim: set ai et sw=2 ts=2 :
+def business_find_or_ask(acc, ask_user: false)
+  match_bid = $cfg.get('business.id', :internal)
+  match_name = $cfg.get('business.name', :internal)
+  match_either = $cfg.get('business.mark', :internal)
+  unless match_bid.to_s.empty? && match_name.to_s.empty? && match_either.to_s.empty?
+    biz = business_locate!(acc, match_bid, match_name, match_either)
+  else
+    biz = business_from_config unless ask_user
+    biz = businesses_ask_which if biz.nil?
+  end
+  # Save the 'business.id' and 'business.name' to the project config.
+  biz.write
+end
+
+def business_locate!(acc, match_bid, match_name, match_either)
+  biz = nil
+  bizes = acc.businesses(match_bid, match_name, match_either)
+  if bizes.count == 1
+    biz = bizes.first
+    say("Found business #{biz.pretty_name_and_id}")
+    puts('')
+  elsif bizes.count > 1
+    acc.error('More than one matching business was found. Please be more specific.')
+    exit(1)
+  else
+    acc.error('No matching business was found. Please try again.')
+    #say('Please visit Exosite.com to view your account and to create a business:')
+    #say("  #{MrMurano::SIGN_UP_URL}")
+    exit(1)
+  end
+  biz
+end
+
+def business_from_config
+  # By default, creating a new Business object loads its ID from the config.
+  biz = MrMurano::Business.new
+  unless biz.bid.empty?
+    # Verify that the business exists.
+    MrMurano::Verbose.whirly_start('Verifying Business...')
+    biz.overview
+    MrMurano::Verbose.whirly_stop
+    if biz.valid?
+      say("Found Business #{biz.pretty_name_and_id}")
+    else
+      say("Could not find Business ‘#{biz.bid}’ referenced in the config")
+    end
+    puts('')
+  end
+  biz if biz.valid?
+end
+
+def businesses_ask_which(acc)
+  biz = nil
+  bizes = acc.businesses
+  if bizes.count == 1
+    biz = bizes.first
+    say("This user has one business. Using #{biz.pretty_name_and_id}")
+  elsif bizes.count.zero?
+    acc.warning('This user has not created any businesses.')
+    say('Please log on to exosite.com to create a free account. Visit:')
+    say("  #{MrMurano::SIGN_UP_URL}")
+    exit 3
+  else
+    choose do |menu|
+      menu.prompt = 'Please select the Business to use:'
+      menu.flow = :columns_across
+      bizes.sort_by { |a| a.name }.each do |choice|
+        menu.choice(choice.name) do
+          biz = choice
+        end
+      end
+    end
+  end
+  puts('')
+  biz
+end
 
