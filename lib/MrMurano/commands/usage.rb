b6e812f0fc16a692c245d5550b6aaac63b8340cf
landonb <landonb@users.noreply.github.com>
2017-07-02 11:41:44 -0500

    Make usage.rb Rubocop compliant; call new c.verify_arg_count!; call new Solution.pretty_desc.

--- lib/MrMurano/commands/usage.rb
+++ lib/MrMurano/commands/usage.rb
@@ -1,34 +1,41 @@
+# Last Modified: 2017.07.01 /coding: utf-8
+# frozen_string_literal: true
+
+# Copyright © 2016-2017 Exosite LLC.
+# License: MIT. See LICENSE.txt.
+#  vim:tw=0:ts=2:sw=2:et:ai
+
 require 'MrMurano/Solution'
 
 command :usage do |c|
-  c.syntax = %{murano usage}
-  c.summary = %{Get usage info for solution(s)}
-  c.description = %{
+  c.syntax = %(murano usage)
+  c.summary = %(Get usage info for solution(s))
+  c.description = %(
 Get usage info for solution(s).
-  }.strip
+  ).strip
   # Add the flags: --types, --ids, --names, --[no]-header.
-  command_add_solution_pickers c
+  command_add_solution_pickers(c)
 
   c.action do |args, options|
+    c.verify_arg_count!(args)
+
     solz = must_fetch_solutions!(options)
 
     solsages = []
-    MrMurano::Verbose::whirly_start "Fetching usage..."
-    solz.each do |soln|
-      solsages += [[soln, soln.usage,],]
+    MrMurano::Verbose.whirly_start('Fetching usage...')
+    solz.each do |sol|
+      solsages += [[sol, sol.usage]]
     end
-    MrMurano::Verbose::whirly_stop
+    MrMurano::Verbose.whirly_stop
 
-    solsages.each do |soln, usage|
-      soln.outf(usage) do |dd, ios|
-        if options.header
-          ios.puts "#{soln.type}: #{soln.pretty_desc}"
-        end
-        headers = ['', :Quota, :Daily, :Monthly, :Total,]
+    solsages.each do |sol, usage|
+      sol.outf(usage) do |dd, ios|
+        ios.puts(sol.pretty_desc(add_type: true)) if options.header
+        headers = ['', :Quota, :Daily, :Monthly, :Total]
         rows = []
         dd.each_pair do |key, value|
-          quota = value[:quota] or {}
-          usage = value[:usage] or {}
+          quota = value[:quota] || {}
+          usage = value[:usage] || {}
           rows << [
             key,
             quota[:daily],
@@ -37,10 +44,9 @@ Get usage info for solution(s).
             usage[:calls_total],
           ]
         end
-        soln.tabularize({:headers=>headers, :rows=>rows}, ios)
+        sol.tabularize({ headers: headers, rows: rows }, ios)
       end
     end
-
   end
 end
 alias_command 'usage product', 'usage', '--type', 'product'
@@ -51,6 +57,4 @@ alias_command 'usage application', 'usage', '--type', 'application'
 alias_command 'usage applications', 'usage', '--type', 'application'
 alias_command 'usage app', 'usage', '--type', 'application'
 alias_command 'usage apps', 'usage', '--type', 'application'
-
-#  vim: set ai et sw=2 ts=2 :
 
