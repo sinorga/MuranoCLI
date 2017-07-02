e895df08037b80468464b57ddd8494facb51b9e9
landonb <landonb@users.noreply.github.com>
2017-07-02 11:16:00 -0500

    Make sync.rb Rubocop compliant, and rename some camcelCased calls to snake_case.

--- lib/MrMurano/commands/sync.rb
+++ lib/MrMurano/commands/sync.rb
@@ -1,81 +1,86 @@
+# Last Modified: 2017.07.02 /coding: utf-8
+# frozen_string_literal: true
+
+# Copyright © 2016-2017 Exosite LLC.
+# License: MIT. See LICENSE.txt.
+#  vim:tw=0:ts=2:sw=2:et:ai
+
 require 'MrMurano/verbosing'
 
 def sync_add_options(c, locale)
-  c.option '--[no-]delete', %{Don't delete things from #{locale}}
-  c.option '--[no-]create', %{Don't create things on #{locale}}
-  c.option '--[no-]update', %{Don't update things on #{locale}}
+  c.option '--[no-]delete', %(Don't delete things from #{locale})
+  c.option '--[no-]create', %(Don't create things on #{locale})
+  c.option '--[no-]update', %(Don't update things on #{locale})
 end
 
-def syncdownFiles(options, args=nil)
+def syncdown_files(options, args=nil)
   args = [] if args.nil?
-  MrMurano::SyncRoot.each_filtered(options) do |name, type, klass, desc|
-    MrMurano::Verbose::whirly_msg "Syncing #{desc}..."
+  MrMurano::SyncRoot.each_filtered(options) do |_name, _type, klass, desc|
+    MrMurano::Verbose.whirly_msg "Syncing #{desc}..."
     sol = klass.new
     sol.syncdown(options, args)
     sleep 1
   end
-  MrMurano::Verbose::whirly_stop
+  MrMurano::Verbose.whirly_stop
 end
 
 command :syncdown do |c|
-  c.syntax = %{murano syncdown [options] [filters]}
-  c.summary = %{Sync project down from Murano}
-  c.description = %{
+  c.syntax = %(murano syncdown [options] [filters])
+  c.summary = %(Sync project down from Murano)
+  c.description = %(
 Sync project down from Murano.
-  }.strip
+  ).strip
   c.option '--all', 'Sync everything'
 
   # Load options to control which things to sync
-  MrMurano::SyncRoot.each_option do |s,l,d|
+  MrMurano::SyncRoot.each_option do |s, l, d|
     c.option s, l, d
   end
 
-  sync_add_options(c, "local machine")
+  sync_add_options(c, 'local machine')
 
-  c.example %{Make local be like what is on the server}, %{murano syncdown --all}
-  c.example %{Pull down new things, but don't delete or modify anything}, %{murano syncdown --all --no-delete --no-update}
-  c.example %{Only Pull new static files}, %{murano syncdown --files --no-delete --no-update}
+  c.example %(Make local be like what is on the server), %(murano syncdown --all)
+  c.example %(Pull down new things, but don't delete or modify anything), %(murano syncdown --all --no-delete --no-update)
+  c.example %(Only Pull new static files), %(murano syncdown --files --no-delete --no-update)
 
-  c.action do |args,options|
-    options.default :delete=>true, :create=>true, :update=>true
+  c.action do |args, options|
+    options.default delete: true, create: true, update: true
 
-    syncdownFiles(options.__hash__, args)
+    syncdown_files(options.__hash__, args)
   end
 end
 alias_command :pull, :syncdown, '--no-delete'
 
 command :syncup do |c|
-  c.syntax = %{murano syncup [options] [filters]}
-  c.summary = %{Sync project up into Murano}
-  c.description = %{
+  c.syntax = %(murano syncup [options] [filters])
+  c.summary = %(Sync project up into Murano)
+  c.description = %(
 Sync project up into Murano.
-  }.strip
+  ).strip
   c.option '--all', 'Sync everything'
 
   # Load options to control which things to sync
-  MrMurano::SyncRoot.each_option do |s,l,d|
+  MrMurano::SyncRoot.each_option do |s, l, d|
     c.option s, l, d
   end
 
-  sync_add_options(c, "server")
+  sync_add_options(c, 'server')
 
-  c.example %{Deploy project to server}, %{murano syncup --all}
-  c.example %{Update static files}, %{murano syncup --files}
-  c.example %{Only add or modify static files}, %{murano syncup --files --no-delete}
+  c.example %(Deploy project to server), %(murano syncup --all)
+  c.example %(Update static files), %(murano syncup --files)
+  c.example %(Only add or modify static files), %(murano syncup --files --no-delete)
 
-  c.action do |args,options|
-    options.default :delete=>true, :create=>true, :update=>true
+  c.action do |args, options|
+    options.default delete: true, create: true, update: true
 
-    #MrMurano::Verbose::whirly_start "Syncing solutions..."
-    MrMurano::SyncRoot.each_filtered(options.__hash__) do |name, type, klass, desc|
-      MrMurano::Verbose::whirly_msg "Syncing #{desc}..."
+    #MrMurano::Verbose.whirly_start "Syncing solutions..."
+    MrMurano::SyncRoot.each_filtered(options.__hash__) do |_name, _type, klass, desc|
+      MrMurano::Verbose.whirly_msg "Syncing #{desc}..."
       sol = klass.new
       sol.syncup(options, args)
     end
-    MrMurano::Verbose::whirly_stop
+    MrMurano::Verbose.whirly_stop
   end
 end
 alias_command :push, :syncup, '--no-delete'
-
-#  vim: set ai et sw=2 ts=2 :
 
