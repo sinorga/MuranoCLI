e783523187bdee343e66f8b0b4387277a281a51f
landonb <landonb@users.noreply.github.com>
2017-07-03 01:21:35 -0500

    Fix status command rspec, and DRY, and remove :not_in_okami tags.

--- spec/cmd_status_spec.rb
+++ spec/cmd_status_spec.rb
@@ -1,3 +1,10 @@
+# Last Modified: 2017.07.03 /coding: utf-8
+# frozen_string_literal: probably not yet
+
+# Copyright © 2016-2017 Exosite LLC.
+# License: MIT. See LICENSE.txt.
+#  vim:tw=0:ts=2:sw=2:et:ai
+
 require 'fileutils'
 require 'open3'
 require 'pathname'
@@ -8,11 +15,6 @@ RSpec.describe 'murano status', :cmd, :needs_password do
   include_context "CI_CMD"
 
   before(:example) do
-    # With an application only, the expected output is different, e.g.,
-    #   $ murano application create statusTest --save
-    #   $ murano status
-    #   Skipping missing location /tmp/d20170602-25639-1xb3al5/project/modules
-    #   No product!
     @product_name = rname('statusTest')
     out, err, status = Open3.capture3(capcmd('murano', 'product', 'create', @product_name, '--save'))
     expect(err).to eq('')
@@ -29,6 +31,13 @@ RSpec.describe 'murano status', :cmd, :needs_password do
     #expect(out).to a_string_starting_with("Linked product #{@product_name}")
     #expect(err).to eq('')
     #expect(status.exitstatus).to eq(0)
+
+    out, err, status = Open3.capture3(
+      capcmd('murano', 'syncdown', '--eventhandlers', '--no-delete', '--no-update')
+    )
+    expect(err).to eq('')
+    expect(out).to eq('')
+    expect(status.exitstatus).to eq(0)
   end
 
   after(:example) do
@@ -43,20 +52,76 @@ RSpec.describe 'murano status', :cmd, :needs_password do
     expect(status.exitstatus).to eq(0)
   end
 
+  def match_syncable_contents(slice)
+      expect(slice).to include(
+        a_string_matching(/ \+ M  .*modules\/table_util\.lua/),
+        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua/),
+        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:4/),
+        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:7/),
+        # singleRoute only appears in some of the tests.
+        a_string_matching(/ \+ A  .*routes\/singleRoute\.lua/),
+        a_string_matching(/ \+ S  .*files\/js\/script\.js/),
+        a_string_matching(/ \+ S  .*files\/icon\.png/),
+        a_string_matching(/ \+ S  .*files\/index\.html/),
+      )
+  end
+
+  def match_syncable_contents_except_singleRoute(slice)
+      expect(slice).to include(
+        a_string_matching(/ \+ M  .*modules\/table_util\.lua/),
+        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua/),
+        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:4/),
+        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:7/),
+        # singleRoute does not appear in old Solutionfile tests
+        # that don't specify it.
+        #a_string_matching(/ \+ A  .*routes\/singleRoute\.lua/),
+        a_string_matching(/ \+ S  .*files\/js\/script\.js/),
+        a_string_matching(/ \+ S  .*files\/icon\.png/),
+        a_string_matching(/ \+ S  .*files\/index\.html/),
+      )
+  end
+
+  def match_remote_boilerplate_v1_0_0_service(slice)
+    expect(slice).to contain_exactly(
+      " - E  device2_event\n",
+      " - E  interface_addGatewayResource\n",
+      " - E  interface_addIdentity\n",
+      " - E  interface_clearContent\n",
+      " - E  interface_deleteContent\n",
+      " - E  interface_downloadContent\n",
+      " - E  interface_getGatewayResource\n",
+      " - E  interface_getGatewaySettings\n",
+      " - E  interface_getIdentity\n",
+      " - E  interface_getIdentityState\n",
+      " - E  interface_infoContent\n",
+      " - E  interface_listContent\n",
+      " - E  interface_listIdentities\n",
+      " - E  interface_makeIdentity\n",
+      " - E  interface_removeGatewayResource\n",
+      " - E  interface_removeIdentity\n",
+      " - E  interface_setIdentityState\n",
+      " - E  interface_updateGatewayResource\n",
+      " - E  interface_updateGatewaySettings\n",
+      " - E  interface_updateIdentity\n",
+      " - E  interface_uploadContent\n",
+      " - E  timer_timer\n",
+      " - E  tsdb_exportJob\n",
+      " - E  user_account\n",
+    )
+  end
+
   context "without ProjectFile" do
     before(:example) do
       FileUtils.cp_r(File.join(@testdir, 'spec/fixtures/syncable_content/.'), '.')
-      FileUtils.move('assets','files')
+      FileUtils.move('assets', 'files')
       FileUtils.mkpath('specs')
-      FileUtils.copy(File.join(@testdir, 'spec/fixtures/product_spec_files/lightbulb.yaml'),
-        'specs/resources.yaml')
+      FileUtils.copy(
+        File.join(@testdir, 'spec/fixtures/product_spec_files/lightbulb.yaml'),
+        'specs/resources.yaml',
+      )
     end
 
-    # FIXME/2017-06-23: Can maybe remove :not_in_okami if the status command
-    #   is returning what we really expect it to; I just copied all the output
-    #   from running the command, assuming that all the missing files are
-    #   because boilerplate creations. Not confident about "Items that differ", though.
-    it "status", :not_in_okami do
+    it "status" do
       out, err, status = Open3.capture3(capcmd('murano', 'status'))
       expect(err).to eq('')
       # Two problems with this output.
@@ -64,51 +129,20 @@ RSpec.describe 'murano status', :cmd, :needs_password do
       # 2: Path prefixes could be different.
       olines = out.lines
       expect(olines[0]).to eq("Only on local machine:\n")
-      expect(olines[1..8]).to contain_exactly(
-        a_string_matching(/ \+ M  .*modules\/table_util\.lua/),
-        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua/),
-        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:4/),
-        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:7/),
-        a_string_matching(/ \+ A  .*routes\/singleRoute\.lua/),
-        a_string_matching(/ \+ S  .*files\/js\/script\.js/),
-        a_string_matching(/ \+ S  .*files\/icon\.png/),
-        a_string_matching(/ \+ S  .*files\/index\.html/),
-      )
-      expect(olines[9]).to eq("Only on remote server:\n")
+      match_syncable_contents(olines[1..8])
+      #expect(olines[9]).to eq("Only on remote server:\n")
+      expect(olines[9]).to eq("Nothing new remotely\n")
       # FIMXE/2017-06-23: We should DRY this long list which is same in each test.
       # FIXME/2017-06-23: The interfaces the server creates for a new project
       #   will problem vary depending on what modules are loaded, and are likely
       #   to change over time...
-      expect(olines[10..31]).to contain_exactly(
-        " - E  tsdb_exportJob\n",
-        #" - E  timer_timer\n",
-        " - E  user_account\n",
-        " - E  interface_setIdentityState\n",
-        " - E  interface_updateGatewayResource\n",
-        " - E  interface_updateIdentity\n",
-        " - E  interface_updateGatewaySettings\n",
-        " - E  interface_uploadContent\n",
-        " - E  interface_getIdentity\n",
-        " - E  interface_removeIdentity\n",
-        " - E  interface_addIdentity\n",
-        " - E  interface_makeIdentity\n",
-        " - E  interface_addGatewayResource\n",
-        " - E  interface_getGatewayResource\n",
-        " - E  interface_listContent\n",
-        " - E  interface_clearContent\n",
-        " - E  interface_getIdentityState\n",
-        " - E  interface_removeGatewayResource\n",
-        " - E  interface_getGatewaySettings\n",
-        " - E  interface_downloadContent\n",
-        " - E  interface_infoContent\n",
-        " - E  interface_listIdentities\n",
-        " - E  interface_deleteContent\n",
-      )
-      expect(olines[32]).to eq("Items that differ:\n")
-      expect(olines[33..34]).to contain_exactly(
-        a_string_matching(/ M E  .*services\/timers\.lua/),
-        a_string_matching(/ M E  .*services\/devdata\.lua/),
-      )
+      #match_remote_boilerplate_v1_0_0_service(olines[10..31])
+      expect(olines[10]).to eq("Nothing that differs\n")
+      #expect(olines[32]).to eq("Items that differ:\n")
+      #expect(olines[33..34]).to contain_exactly(
+      #  a_string_matching(/ M E  .*services\/timers\.lua/),
+      #  a_string_matching(/ M E  .*services\/devdata\.lua/),
+      #)
       expect(status.exitstatus).to eq(0)
     end
 
@@ -139,67 +173,38 @@ RSpec.describe 'murano status', :cmd, :needs_password do
 
   context "with ProjectFile" do
     before(:example) do
+      # We previously called syncdown, which created the project/services/
+      # directory, but don't fret, this copy command will overlay files and
+      # it will not overwrite directories (or do nothing to them, either).
       FileUtils.cp_r(File.join(@testdir, 'spec/fixtures/syncable_content/.'), '.')
-      FileUtils.move('assets','files')
+      FileUtils.move('assets', 'files')
       FileUtils.mkpath('specs')
-      FileUtils.copy(File.join(@testdir, 'spec/fixtures/product_spec_files/lightbulb.yaml'),
-        'specs/resources.yaml')
+      FileUtils.copy(
+        File.join(@testdir, 'spec/fixtures/product_spec_files/lightbulb.yaml'),
+        'specs/resources.yaml',
+      )
       FileUtils.copy(File.join(@testdir, 'spec/fixtures/ProjectFiles/only_meta.yaml'), 'test.murano')
     end
 
-    it "status", :not_in_okami do
+    it "status" do
       out, err, status = Open3.capture3(capcmd('murano', 'status'))
       expect(err).to eq('')
       olines = out.lines
       expect(olines[0]).to eq("Only on local machine:\n")
-      expect(olines[1..8]).to include(
-        a_string_matching(/ \+ M  .*modules\/table_util\.lua/),
-        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua/),
-        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:4/),
-        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:7/),
-        a_string_matching(/ \+ A  .*routes\/singleRoute\.lua/),
-        a_string_matching(/ \+ S  .*files\/js\/script\.js/),
-        a_string_matching(/ \+ S  .*files\/icon\.png/),
-        a_string_matching(/ \+ S  .*files\/index\.html/),
-      )
-      expect(olines[9]).to eq("Only on remote server:\n")
-      expect(olines[10..31]).to include(
-        " - E  tsdb_exportJob\n",
-        #" - E  timer_timer\n",
-        " - E  user_account\n",
-        " - E  interface_setIdentityState\n",
-        " - E  interface_updateGatewayResource\n",
-        " - E  interface_updateIdentity\n",
-        " - E  interface_updateGatewaySettings\n",
-        " - E  interface_uploadContent\n",
-        " - E  interface_getIdentity\n",
-        " - E  interface_removeIdentity\n",
-        " - E  interface_addIdentity\n",
-        " - E  interface_makeIdentity\n",
-        " - E  interface_addGatewayResource\n",
-        " - E  interface_getGatewayResource\n",
-        " - E  interface_listContent\n",
-        " - E  interface_clearContent\n",
-        " - E  interface_getIdentityState\n",
-        " - E  interface_removeGatewayResource\n",
-        " - E  interface_getGatewaySettings\n",
-        " - E  interface_downloadContent\n",
-        " - E  interface_infoContent\n",
-        " - E  interface_listIdentities\n",
-        " - E  interface_deleteContent\n",
-      )
-      #expect(olines[32]).to eq("Nothing that differs\n")
-      expect(olines[32]).to eq("Items that differ:\n")
-      expect(olines[33..34]).to include(
-        a_string_matching(/ M E  .*services\/timers\.lua/),
-        a_string_matching(/ M E  .*services\/devdata\.lua/),
-      )
+      match_syncable_contents(olines[1..8])
+      expect(olines[9]).to eq("Nothing new remotely\n")
+      expect(olines[10]).to eq("Nothing that differs\n")
+      #expect(olines[10]).to eq("Items that differ:\n")
+      #expect(olines[11..12]).to include(
+      #  a_string_matching(/ M E  .*services\/timers\.lua/),
+      #  a_string_matching(/ M E  .*services\/devdata\.lua/),
+      #)
       expect(status.exitstatus).to eq(0)
     end
   end
 
   # XXX wait, should a Solutionfile even work with Okami?
-  context "with Solutionfile 0.2.0", :not_in_okami do
+  context "with Solutionfile 0.2.0" do
     before(:example) do
       FileUtils.cp_r(File.join(@testdir, 'spec/fixtures/syncable_content/.'), '.')
       FileUtils.move('assets','files')
@@ -229,62 +234,34 @@ RSpec.describe 'murano status', :cmd, :needs_password do
       # Not a single match, because the order of items within groups can shift
       olines = out.lines
       expect(olines[0]).to eq("Only on local machine:\n")
-      expect(olines[1..7]).to contain_exactly(
-        a_string_matching(/ \+ M  .*modules\/table_util\.lua/),
-        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua/),
-        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:4/),
-        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:7/),
-        a_string_matching(/ \+ S  .*files\/index\.html/),
-        a_string_matching(/ \+ S  .*files\/js\/script\.js/),
-        a_string_matching(/ \+ S  .*files\/icon\.png/),
-      )
+      match_syncable_contents_except_singleRoute(olines[1..7])
       expect(olines[8]).to eq("Only on remote server:\n")
-      expect(olines[9..31]).to contain_exactly(
-        " - E  tsdb_exportJob\n",
-        " - E  timer_timer\n",
-        " - E  user_account\n",
-        " - E  interface_setIdentityState\n",
-        " - E  interface_updateGatewayResource\n",
-        " - E  interface_updateIdentity\n",
-        " - E  interface_updateGatewaySettings\n",
-        " - E  interface_uploadContent\n",
-        " - E  interface_getIdentity\n",
-        " - E  interface_removeIdentity\n",
-        " - E  interface_addIdentity\n",
-        " - E  interface_makeIdentity\n",
-        " - E  interface_addGatewayResource\n",
-        " - E  interface_getGatewayResource\n",
-        " - E  interface_listContent\n",
-        " - E  interface_clearContent\n",
-        " - E  interface_getIdentityState\n",
-        " - E  interface_removeGatewayResource\n",
-        " - E  interface_getGatewaySettings\n",
-        " - E  interface_downloadContent\n",
-        " - E  interface_infoContent\n",
-        " - E  interface_listIdentities\n",
-        " - E  interface_deleteContent\n",
-      )
-      expect(olines[32]).to eq("Items that differ:\n")
-      expect(olines[33..33]).to contain_exactly(
-        a_string_matching(/ M E  .*services\/devdata\.lua/),
-      )
+      match_remote_boilerplate_v1_0_0_service(olines[9..32])
+      expect(olines[33]).to eq("Nothing that differs\n")
+      #expect(olines[32]).to eq("Items that differ:\n")
+      #expect(olines[33..33]).to contain_exactly(
+      #  a_string_matching(/ M E  .*services\/devdata\.lua/),
+      #)
       expect(status.exitstatus).to eq(0)
     end
   end
 
   # XXX wait, should a Solutionfile even work with Okami?
-  context "with Solutionfile 0.3.0", :not_in_okami do
+  context "with Solutionfile 0.3.0" do
     before(:example) do
       FileUtils.cp_r(File.join(@testdir, 'spec/fixtures/syncable_content/.'), '.')
       FileUtils.move('assets','files')
       FileUtils.mkpath('specs')
-      FileUtils.copy(File.join(@testdir, 'spec/fixtures/product_spec_files/lightbulb.yaml'),
-        'specs/resources.yaml')
+      FileUtils.copy(
+        File.join(@testdir, 'spec/fixtures/product_spec_files/lightbulb.yaml'),
+        'specs/resources.yaml',
+      )
       File.open('Solutionfile.json', 'wb') do |io|
         io << {
           :default_page => 'index.html',
           :assets => 'files',
           :routes => 'routes/manyRoutes.lua',
+          # Note that singleRoute.lua is not included, so it won't be seen by status command.
           :modules => {
             :table_util => 'modules/table_util.lua'
           },
@@ -300,54 +277,20 @@ RSpec.describe 'murano status', :cmd, :needs_password do
 
     it "status" do
       out, err, status = Open3.capture3(capcmd('murano', 'status'))
+      # pp out.split "\n"
       expect(err).to eq('')
       olines = out.lines
       expect(olines[0]).to eq("Only on local machine:\n")
-      expect(olines[1..7]).to contain_exactly(
-        a_string_matching(/ \+ M  .*modules\/table_util\.lua/),
-        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua/),
-        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:4/),
-        a_string_matching(/ \+ A  .*routes\/manyRoutes\.lua:7/),
-        a_string_matching(/ \+ S  .*files\/js\/script\.js/),
-        a_string_matching(/ \+ S  .*files\/icon\.png/),
-        a_string_matching(/ \+ S  .*files\/index\.html/),
-      )
+      match_syncable_contents_except_singleRoute(olines[1..7])
       expect(olines[8]).to eq("Only on remote server:\n")
-      expect(olines[9..31]).to contain_exactly(
-        " - E  tsdb_exportJob\n",
-        " - E  timer_timer\n",
-        " - E  user_account\n",
-        " - E  interface_setIdentityState\n",
-        " - E  interface_updateGatewayResource\n",
-        " - E  interface_updateIdentity\n",
-        " - E  interface_updateGatewaySettings\n",
-        " - E  interface_uploadContent\n",
-        " - E  interface_getIdentity\n",
-        " - E  interface_removeIdentity\n",
-        " - E  interface_addIdentity\n",
-        " - E  interface_makeIdentity\n",
-        " - E  interface_addGatewayResource\n",
-        " - E  interface_getGatewayResource\n",
-        " - E  interface_listContent\n",
-        " - E  interface_clearContent\n",
-        " - E  interface_getIdentityState\n",
-        " - E  interface_removeGatewayResource\n",
-        " - E  interface_getGatewaySettings\n",
-        " - E  interface_downloadContent\n",
-        " - E  interface_infoContent\n",
-        " - E  interface_listIdentities\n",
-        " - E  interface_deleteContent\n",
-      )
-      #expect(olines[32]).to eq("Nothing that differs\n")
-      expect(olines[32]).to eq("Items that differ:\n")
-      expect(olines[33..33]).to contain_exactly(
-        a_string_matching(/ M E  .*services\/devdata\.lua/),
-      )
+      match_remote_boilerplate_v1_0_0_service(olines[9..32])
+      expect(olines[33]).to eq("Nothing that differs\n")
+      #expect(olines[33]).to eq("Items that differ:\n")
+      #expect(olines[34..34]).to contain_exactly(
+      #  a_string_matching(/ M E  .*services\/devdata\.lua/),
+      #)
       expect(status.exitstatus).to eq(0)
     end
   end
-
 end
-
-#  vim: set ai et sw=2 ts=2 :
 
