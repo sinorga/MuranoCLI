92530e670aea4671e0b1478e7dc2728bad4ed340
landonb <landonb@users.noreply.github.com>
2017-07-13 11:49:30 -0600

    Bugfix: murano show. Need to use new Business object to get Applications and Products. MRMUR-126
    - Also, lint the file (Rubocop compliance).
    - Also, add Whirly usage around solution fetches.

--- lib/MrMurano/commands/show.rb
+++ lib/MrMurano/commands/show.rb
@@ -1,3 +1,9 @@
+# Last Modified: 2017.07.13 /coding: utf-8
+# frozen_string_literal: true
+
+# Copyright © 2016-2017 Exosite LLC.
+# License: MIT. See LICENSE.txt.
+#  vim:tw=0:ts=2:sw=2:et:ai
 
 command 'show' do |c|
   c.syntax = %(murano show)
@@ -7,70 +13,69 @@ Show readable information about the current configuration.
   ).strip
   c.project_not_required = true
 
-  c.action do |args, options|
-
-    if args.include?('help') then
+  c.action do |args, _options|
+    if args.include?('help')
       ::Commander::UI.enable_paging
       say MrMurano::SubCmdGroupHelp.new(c).get_help
     else
       acc = MrMurano::Account.instance
+      biz = MrMurano::Business.new
 
-      selectedBusinessId = $cfg['business.id']
-      selectedBusiness = nil
-      acc.businesses.each do |row|
-        selectedBusiness = row if row[:bizid] == selectedBusinessId
+      selected_business_id = $cfg['business.id']
+      selected_business = nil
+      acc.businesses.each do |sol|
+        selected_business = sol if sol.bizid == selected_business_id
       end
 
-      selectedProductId = $cfg['product.id']
-      selectedProduct = nil
-      acc.products.each do |row|
-        selectedProduct = row if row[:apiId] == selectedProductId
+      MrMurano::Verbose.whirly_start('Fetching Applications...')
+      selected_application_id = $cfg['application.id']
+      selected_application = nil
+      biz.applications.each do |sol|
+        selected_application = sol if sol.apiId == selected_application_id
       end
+      MrMurano::Verbose.whirly_stop
 
-      selectedApplicationId = $cfg['application.id']
-      selectedApplication = nil
-      acc.applications.each do |row|
-        selectedApplication = row if row[:apiId] == selectedApplicationId
+      MrMurano::Verbose.whirly_start('Fetching Products...')
+      selected_product_id = $cfg['product.id']
+      selected_product = nil
+      biz.products.each do |sol|
+        selected_product = sol if sol.apiId == selected_product_id
       end
+      MrMurano::Verbose.whirly_stop
 
-      if $cfg['user.name'] then
+      if $cfg['user.name']
         puts %(user: #{$cfg['user.name']})
       else
         puts 'no user selected'
       end
 
-      if selectedBusiness then
-        puts %(business: #{selectedBusiness[:name]})
+      if selected_business
+        puts %(business: #{selected_business.name})
       else
         puts 'no business selected'
-      end
-
-      # E.g., {:bizid=>"AAAAAAAAAAAAAAAA", :type=>"product",
-      #   :apiId=>"BBBBBBBBBBBBBBBBB", :sid=>"BBBBBBBBBBBBBBBBB",
-      #   :domain=>"BBBBBBBBBBBBBBBBB.m2.exosite.io", :name=>"AAAAAAAAAAAAAAAA"}
-      if selectedProduct then
-        puts %(product: #{selectedProduct[:name]})
-      else
-        if selectedProductId then
-          puts 'selected product not in business'
-        else
-          puts 'no product selected'
-        end
       end
 
       # E.g., {:bizid=>"AAAAAAAAAAAAAAAA", :type=>"application",
       #   :apiId=>"CCCCCCCCCCCCCCCCC", :sid=>"CCCCCCCCCCCCCCCCC",
       #   :domain=>"AAAAAAAAAAAAAAAA.apps.exosite.io", :name=>"AAAAAAAAAAAAAAAA"}
-      if selectedApplication then
-        puts %(application: https://#{selectedApplication[:domain]})
+      if selected_application
+        puts %(application: https://#{selected_application.domain})
+      elsif selected_application_id
+        puts 'selected application not in business'
       else
-        if selectedApplicationId then
-          puts 'selected application not in business'
-        else
-          puts 'no application selected'
-        end
+        puts 'no application selected'
       end
 
+      # E.g., {:bizid=>"AAAAAAAAAAAAAAAA", :type=>"product",
+      #   :apiId=>"BBBBBBBBBBBBBBBBB", :sid=>"BBBBBBBBBBBBBBBBB",
+      #   :domain=>"BBBBBBBBBBBBBBBBB.m2.exosite.io", :name=>"AAAAAAAAAAAAAAAA"}
+      if selected_product
+        puts %(product: #{selected_product.name})
+      elsif selected_product_id
+        puts 'selected product not in business'
+      else
+        puts 'no product selected'
+      end
     end
   end
 end
@@ -83,11 +88,9 @@ Show readable information about the current configuration.
   ).strip
   c.project_not_required = true
 
-  c.action do |args, options|
+  c.action do |_args, _options|
     puts %(base: #{$cfg['location.base']})
     $cfg['location'].each { |key, value| puts %(#{key}: #{$cfg['location.base']}/#{value}) }
   end
 end
-
-#  vim: set ai et sw=2 ts=2 :
 
