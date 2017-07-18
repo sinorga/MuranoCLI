ec48e4e4fe820d335ed265e5df3087d5dd8d32c4
landonb <landonb@users.noreply.github.com>
2017-07-18 17:27:02 -0500

    Fix status and diff command to work with both --asdown and --no-asdown, and to report diff appropriately. (Issue: MRMUR-133)
    - Both commands were failing when --no-asdown was specified.
    - Diff would always diff local against remote, so whether --asdown or --no-asdown,
      the diff would always show the same thing; no the diff is ordered appropriately,
      so with --asdown, diff shows what remote changes replace when local lines, and
      when --no-asdown is specified, diff shows what local changes replace what remote
      lines.

--- lib/MrMurano/Gateway.rb
+++ lib/MrMurano/Gateway.rb
@@ -203,6 +203,23 @@ module MrMurano
         end
       end
 
+      def diff_download(tmp_path, merged)
+        @there = list if @there.nil?
+        items = @there.reject { |item| item[:alias] != merged[:alias] }
+        if items.length > 1
+          error(
+            "Unexpected: more than 1 resource with the same alias: #{merged[:alias]} / #{items}"
+          )
+        end
+        Pathname.new(tmp_path).open('wb') do |io|
+          if !items.length.zero?
+            diff_item_write(io, merged, nil, items.first)
+          else
+            io << "\n"
+          end
+        end
+      end
+
       def removelocal(_local, item)
         # needs to append/merge with file
         key = @itemkey.to_sym
@@ -239,12 +256,14 @@ module MrMurano
         io << res.to_yaml
       end
 
-      def diff_local_write(io, _merged, local)
-        raise "Unexpected: :local_path exists: #{local}" unless local[:local_path].to_s.empty?
+      def diff_item_write(io, _merged, local, remote)
+        raise 'Unexpected: please specify either local or remote, but not both' if local && remote
+        item = local || remote
+        raise "Unexpected: :local_path exists: #{item}" unless item[:local_path].to_s.empty?
         res = {}
-        key = local[:alias]
-        local = local.reject { |k, _v| k == :alias || k == :synckey }
-        res[key] = Hash.transform_keys_to_strings(local)
+        key = item[:alias]
+        item = item.reject { |k, _v| k == :alias || k == :synckey }
+        res[key] = Hash.transform_keys_to_strings(item)
         io << res.to_yaml
       end
 
