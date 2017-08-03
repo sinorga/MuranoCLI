ec65de3abc25c08b77d3572f5611c0e7a72082e0
landonb <landonb@users.noreply.github.com>
2017-08-03 00:55:16 -0500

    rspec does not appreciate the plus '+' sign in the (semantic) version number. (Issue: MRMUR-141)

--- lib/MrMurano/version.rb
+++ lib/MrMurano/version.rb
@@ -1,4 +1,4 @@
-# Last Modified: 2017.08.02 /coding: utf-8
+# Last Modified: 2017.08.03 /coding: utf-8
 # frozen_string_literal: true
 
 # Copyright © 2016-2017 Exosite LLC.
@@ -6,7 +6,7 @@
 #  vim:tw=0:ts=2:sw=2:et:ai
 
 module MrMurano
-  VERSION = '3.0.0-beta.4+1'
+  VERSION = '3.0.0-beta.4.1'
   EXE_NAME = File.basename($PROGRAM_NAME)
   SIGN_UP_URL = 'https://exosite.com/signup/'
 end
