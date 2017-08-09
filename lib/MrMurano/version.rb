3992045e7f68e95b43ceb4dcc9a8f6ff0aa0349a
landonb <landonb@users.noreply.github.com>
2017-08-09 02:42:13 -0500

    Clean up comment on versioning. (Issue: MRMUR-141)

--- lib/MrMurano/version.rb
+++ lib/MrMurano/version.rb
@@ -1,4 +1,4 @@
-# Last Modified: 2017.08.07 /coding: utf-8
+# Last Modified: 2017.08.09 /coding: utf-8
 # frozen_string_literal: true
 
 # Copyright © 2016-2017 Exosite LLC.
@@ -6,14 +6,26 @@
 #  vim:tw=0:ts=2:sw=2:et:ai
 
 module MrMurano
-  # NOTE: `rake build` (a task added by postmodern/rubygems-tasks)
-  #   replaces the dash in the version with 'rep.', e.g.,
-  #     '3.0.0-beta.4.2' changes '3.0.0.pre.beta.4.2'
-  #   The build task is from https://github.com/postmodern/rubygems-tasks
-  #   The 'dash' in a version is part of http://semver.org/
-  # 2017-08-07: We've been cutting GitHub releases for each beta.X,
-  #   but not for any beta.X.pre.Y or beta.X-Y
-  # NOTE: 3.0.0.beta.5.pre.4 < 3.0.0.beta.5
+  # USAGE: Use 'beta.' and 'pre.' to label beta releases and dev builds.
+  #   For example, the first beta for a 3.0.0 release would be:
+  #     3.0.0.beta.1
+  #   After the beta release, developers would want to use a pre-build:
+  #     3.0.0.beta.2.pre.1
+  #   And then the next beta release drops the 'pre'-postfix:
+  #     3.0.0.beta.2
+  #   And finally the general release drops the 'beta'-postfix:
+  #     3.0.0
+  #   This works because of how versions are ordered, e.g.,
+  #     3.0.0.beta.1 < 3.0.0.beta.2.pre.1 < 3.0.0.beta.2 < 3.0.0
+  #
+  #   Note that we cannot follow the Semantic Version guidelines
+  #   from http://semver.org/. The guidelines say to use a dash to
+  #   separate the pre-release version, and a plus to separate the
+  #   build metadata, e.g., '3.0.0-beta.2+1'. However, the `rake build`
+  #   task replaces a dash in the version with the text, 'pre.', e.g.,
+  #     '3.0.0-beta.2' is changed to '3.0.0.pre.beta.2'
+  #   which breaks our build (which expects the version to match herein).
+  #   So stick to using the '.pre.X' syntax, which ruby/gems knows.
   VERSION = '3.0.0.beta.5.pre.1'
   EXE_NAME = File.basename($PROGRAM_NAME)
   SIGN_UP_URL = 'https://exosite.com/signup/'
