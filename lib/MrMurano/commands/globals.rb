2a5a3136776bbef4cf97fcd5367b1a2654faa206
landonb <landonb@users.noreply.github.com>
2017-08-16 18:03:21 -0500

    Remove the removed global business and solution picker options. (Issue: MRMUR-147).
    - Also reorder the options logically, so they're group somewhat in the --help.

--- lib/MrMurano/commands/globals.rb
+++ lib/MrMurano/commands/globals.rb
@@ -1,31 +1,15 @@
-# Last Modified: 2017.07.26 /coding: utf-8
+# Last Modified: 2017.08.16 /coding: utf-8
 # frozen_string_literal: true
 
 # Copyright © 2016-2017 Exosite LLC.
 # License: MIT. See LICENSE.txt.
 #  vim:tw=0:ts=2:sw=2:et:ai
 
-global_option('-V', '--verbose', %(Be chatty)) do
-  $cfg['tool.verbose'] = true
-end
+require 'MrMurano/Config'
 
-global_option('-n', '--dry', %(Don't run actions that make changes)) do
-  $cfg['tool.dry'] = true
-  # Running dry implies verbose.
-  $cfg['tool.verbose'] = true
-end
-
-global_option('-L', '--curl', %(Print out a curl command for each network call)) do
-  $cfg['tool.curldebug'] = true
-end
-
-global_option '--skip-plugins', %(Don't load plugins. Good for when one goes bad) do
-  # no-op
-end
-
-global_option('-C', '--configfile FILE', %(Load additional configuration file)) do |file|
-  # This is called after all of the top level code in this file.
-  $cfg.load_specific(file)
+global_option('--[no-]color', %(Disable fancy output)) do |value|
+  HighLine.use_color = value
+  Rainbow.enabled = value
 end
 
 global_option('-c', '--config KEY=VALUE', %(Set a single config key)) do |param|
@@ -41,13 +25,19 @@ global_option('-c', '--config KEY=VALUE', %(Set a single config key)) do |param|
   end
 end
 
-global_option('--[no-]color', %(Disable fancy output)) do |value|
-  HighLine.use_color = value
-  Rainbow.enabled = value
+global_option('-C', '--configfile FILE', %(Load additional configuration file)) do |file|
+  # This is called after all of the top level code in this file.
+  $cfg.load_specific(file)
 end
 
-global_option('--[no-]progress', %(Disable spinner and progress message)) do |value|
-  $cfg['tool.no-progress'] = !value
+global_option('-L', '--curl', %(Print out a curl command for each network call)) do
+  $cfg['tool.curldebug'] = true
+end
+
+global_option('-n', '--dry', %(Do not run actions that make changes)) do
+  $cfg['tool.dry'] = true
+  # Running dry implies verbose.
+  $cfg['tool.verbose'] = true
 end
 
 exclude_help = %(
@@ -59,64 +49,16 @@ global_option('--exclude-scopes SCOPES', Array, exclude_help) do |value|
   $cfg.exclude_scopes = value.map(&:to_sym)
 end
 
-# 2017-06-30: Being more flexible. And more consistent. Some commands
-# just use the IDs found in config, but some commands allow the user
-# to pass solution names or IDs as arguments to the command, and some
-# commands should be able to work without being run from inside a project
-# directory.
+# --no-page is handled early on, in bin/murano.
+global_option('--no-page', %(Do not page --help output))
 
-# The user can indicate the business, application, and product via command line.
-# If not specified, we'll look in the config. Or, if the user is running the
-# init command, we'll interact with the user.
+global_option('--[no-]plugins', %(Do not load plugins. Good for when one goes bad))
 
-global_option('--business BUSINESS', String, %(Name or ID of Murano Business to use)) do |business_mark|
-  $cfg['business.id'] = nil
-  $cfg['business.name'] = nil
-  $cfg['business.mark'] = business_mark
-end
-global_option('--business-name NAME', String, %(Name of Murano Business to use)) do |business_name|
-  $cfg['business.id'] = nil
-  $cfg['business.name'] = business_name
-  $cfg['business.mark'] = nil
-end
-global_option('--business-id ID', String, %(ID of Murano Business to use)) do |business_id|
-  $cfg['business.id'] = business_id
-  $cfg['business.name'] = nil
-  $cfg['business.mark'] = nil
+global_option('--[no-]progress', %(Disable spinner and progress message)) do |value|
+  $cfg['tool.no-progress'] = !value
 end
 
-global_option('--application[=APPLICATION]', String, %(Name or ID of Application to use)) do |application_mark|
-  # NOTE: If user does not specify an argument,
-  #         e.g., `murano domain --application`,
-  #       then this block *is not* called.
-  $cfg['application.id'] = nil
-  $cfg['application.name'] = nil
-  $cfg['application.mark'] = application_mark
-end
-global_option('--application-name NAME', String, %(Name of Application to use)) do |application_name|
-  $cfg['application.id'] = nil
-  $cfg['application.name'] = application_name
-  $cfg['application.mark'] = nil
-end
-global_option('--application-id ID', String, %(ID of Application to use)) do |application_id|
-  $cfg['application.id'] = application_id
-  $cfg['application.name'] = nil
-  $cfg['application.mark'] = nil
-end
-
-global_option('--product[=PRODUCT]', String, %(Name or ID of Product to use)) do |product_mark|
-  $cfg['product.id'] = nil
-  $cfg['product.name'] = nil
-  $cfg['product.mark'] = product_mark
-end
-global_option('--product-name NAME', String, %(Name of Product to use)) do |product_name|
-  $cfg['product.id'] = nil
-  $cfg['product.name'] = product_name
-  $cfg['product.mark'] = nil
-end
-global_option('--product-id ID', String, %(ID of Product to use)) do |product_id|
-  $cfg['product.id'] = product_id
-  $cfg['product.name'] = nil
-  $cfg['product.mark'] = nil
+global_option('-V', '--verbose', %(Be chatty)) do
+  $cfg['tool.verbose'] = true
 end
 
