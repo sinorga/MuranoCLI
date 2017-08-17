341a0ff7fcaa4b84eacf5793dcb6415c2d61617e
landonb <landonb@users.noreply.github.com>
2017-08-17 01:18:50 -0500

    Fix rspec tests.

--- spec/cmd_syncup_spec.rb
+++ spec/cmd_syncup_spec.rb
@@ -1,4 +1,4 @@
-# Last Modified: 2017.08.16 /coding: utf-8
+# Last Modified: 2017.08.17 /coding: utf-8
 # frozen_string_literal: probably not yet
 
 # Copyright © 2016-2017 Exosite LLC.
@@ -73,26 +73,27 @@ RSpec.describe 'murano syncup', :cmd, :needs_password do
       out, err, status = Open3.capture3(capcmd('murano', 'syncup'))
       outl = out.lines
       # The spec tests set --no-progress, so each sync action gets reported.
-      expect(outl[0]).to eq("Adding item table_util\n")
-      #expect(outl[1]).to eq("Updating item c3juj9vnmec000000_event\n")
+      #expect(outl[0]).to eq("Adding item state\n")
+      #expect(outl[1]).to eq("Adding item temperature\n")
+      #expect(outl[2]).to eq("Adding item uptime\n")
+      #expect(outl[3]).to eq("Adding item humidity\n")
+      (0..3).each { |ln| expect(outl[ln]).to start_with("Adding item ") }
+      expect(outl[4]).to eq("Updating product resources\n")
+      expect(outl[5]).to eq("Adding item table_util\n")
+      #expect(outl[6]).to eq("Updating item c3juj9vnmec000000_event\n")
       # The order isn't always consistent, so just do start_with.
-      #expect(outl[2]).to eq("Updating item tsdb_exportJob\n")
-      #expect(outl[3]).to eq("Updating item timer_timer\n")
-      #expect(outl[4]).to eq("Updating item user_account\n")
-      (1..4).each { |ln| expect(outl[ln]).to start_with("Updating item ") }
-      #expect(outl[5]).to eq("Adding item POST_/api/fire\n")
-      #expect(outl[6]).to eq("Adding item PUT_/api/fire/{code}\n")
-      #expect(outl[7]).to eq("Adding item DELETE_/api/fire/{code}\n")
-      #expect(outl[8]).to eq("Adding item GET_/api/onfire\n")
-      #expect(outl[9]).to eq("Adding item /js/script.js\n")
-      #expect(outl[10]).to eq("Adding item /icon.png\n")
-      #expect(outl[11]).to eq("Adding item /\n")
-      #expect(outl[12]).to eq("Adding item state\n")
-      #expect(outl[13]).to eq("Adding item temperature\n")
-      #expect(outl[14]).to eq("Adding item uptime\n")
-      #expect(outl[15]).to eq("Adding item humidity\n")
-      (5..15).each { |ln| expect(outl[ln]).to start_with("Adding item ") }
-      expect(outl[16]).to eq("Updating product resources\n")
+      #expect(outl[7]).to eq("Updating item timer_timer\n")
+      #expect(outl[8]).to eq("Updating item user_account\n")
+      #expect(outl[9]).to eq("Updating item tsdb_exportJob\n")
+      (6..9).each { |ln| expect(outl[ln]).to start_with("Updating item ") }
+      #expect(outl[10]).to eq("Adding item POST_/api/fire\n")
+      #expect(outl[11]).to eq("Adding item PUT_/api/fire/{code}\n")
+      #expect(outl[12]).to eq("Adding item DELETE_/api/fire/{code}\n")
+      #expect(outl[13]).to eq("Adding item GET_/api/onfire\n")
+      #expect(outl[14]).to eq("Adding item /icon.png\n")
+      #expect(outl[15]).to eq("Adding item /\n")
+      #expect(outl[16]).to eq("Adding item /js/script.js\n")
+      (10..16).each { |ln| expect(outl[ln]).to start_with("Adding item ") }
       verify_err_missing_location(err)
       expect(status.exitstatus).to eq(0)
 
