14a8a4c408bce446dcab2ea3230fe1ca702984f1
landonb <landonb@users.noreply.github.com>
2017-08-17 11:56:56 -0500

    Lint: lib/MrMurano/Webservice-Cors.rb

--- lib/MrMurano/Webservice-Cors.rb
+++ lib/MrMurano/Webservice-Cors.rb
@@ -1,3 +1,10 @@
+# Last Modified: 2017.08.17 /coding: utf-8
+# frozen_string_literal: true
+
+# Copyright © 2016-2017 Exosite LLC.
+# License: MIT. See LICENSE.txt.
+#  vim:tw=0:ts=2:sw=2:et:ai
+
 require 'json'
 require 'yaml'
 require 'MrMurano/Webservice'
@@ -9,14 +16,16 @@ module MrMurano
         super
         @uriparts << :cors
       end
+
       def cors
-        ret = get('')
         return {} if ret.nil?
         return {} unless ret.kind_of? Hash
-        return ret
+        ret = get
+        ret
       end
+
       def cors=(x)
-        raise "Not Hash" unless x.kind_of? Hash
+        raise 'Not Hash' unless x.is_a? Hash
         put('', x)
       end
     end
@@ -28,16 +37,16 @@ module MrMurano
         #@project_section = :cors
       end
 
-      def fetch(id=nil, &block)
-        ret = get()
         return [] if ret.is_a?(Hash) and ret.has_key?(:error)
-        if ret.kind_of?(Hash) and ret.has_key?(:cors) then
+      def fetch(_id=nil)
+        ret = get
+        if ret.is_a?(Hash) && ret.key?(:cors)
           # XXX cors is a JSON encoded string. That seems weird. keep an eye on this.
           data = JSON.parse(ret[:cors], @json_opts)
         else
           data = ret
         end
-        if block_given? then
+        if block_given?
           yield Hash.transform_keys_to_strings(data).to_yaml
         else
           data
@@ -48,18 +57,16 @@ module MrMurano
       # Upload CORS
       # @param file [String,Nil] File path to upload other than defaults
       def upload(file=nil)
-        unless file.nil? then
+        if !file.nil?
           data = YAML.load_file(file)
         else
           data = $project['routes.cors']
           # If it is just a string, then is a file to load.
-          data = YAML.load_file(data) if data.kind_of? String
+          data = YAML.load_file(data) if data.is_a? String
         end
         put('', data)
       end
-
     end
   end
 end
 
-#  vim: set ai et sw=2 ts=2 :
